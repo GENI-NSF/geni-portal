cba3ad10501db048c4c02177ccc8e9e4ccd41582
Tom Mitchell <tmitchell@bbn.com>
2011-05-06 15:15:51 -0400

    Create prep-ch script for base install.
    Remove the old sbin dir and push it all into prep-ch.sh.

---
+++ prep-ch.sh
@@ -0,0 +1,97 @@
+#!/bin/sh
+
+# A script to configure a GPO lab VM with the right stuff
+# to allow the prototype clearinghouse to run.
+
+check_errs()
+{
+  # Function. Parameter 1 is the return code
+  # Para. 2 is text to display on failure.
+  if [ "${1}" -ne "0" ]; then
+    echo "ERROR # ${1} : ${2}"
+    # as a bonus, make our script exit with the right error code.
+    exit ${1}
+  fi
+}
+
+# XXX php-postgres? pear-php?
+PKGS="postgresql git-core apache2"
+
+/usr/bin/apt-get update
+check_errs $? "apt-get failed to update"
+
+/usr/bin/apt-get -y dist-upgrade
+check_errs $? "apt-get failed to dist-upgrade"
+
+/usr/bin/apt-get install -y ${PKGS}
+check_errs $? "apt-get failed to install packages"
+
+# Set postgres password
+echo
+echo
+echo "@@@@@@@@@@@@@@@@@@@@@"
+echo "@"
+echo "@ Please enter a database password for the 'postgres' user"
+echo "@"
+/usr/bin/sudo -u postgres /usr/bin/psql -c \\password
+
+# Enable postgres accessibility in postgres.conf and pg_hba.conf
+#----------------------------------------------------------------------
+# Patch postgresql.conf to listen for network connections
+#----------------------------------------------------------------------
+/usr/bin/patch --backup -p 0 /etc/postgresql/8.4/main/postgresql.conf <<EOF
+--- postgresql.conf.orig	2011-01-07 13:20:50.840787089 -0500
++++ postgresql.conf	2011-01-07 13:23:10.984792098 -0500
+@@ -60,6 +60,7 @@
+ 					# comma-separated list of addresses;
+ 					# defaults to 'localhost', '*' = all
+ 					# (change requires restart)
++listen_addresses = '*'
+ port = 5432				# (change requires restart)
+ max_connections = 100			# (change requires restart)
+ # Note:  Increasing max_connections costs ~400 bytes of shared memory per 
+EOF
+check_errs $? "failed to patch postgresql.conf"
+
+#----------------------------------------------------------------------
+# Patch pg_hba.conf to allow md5 authentication from anywhere
+#----------------------------------------------------------------------
+/usr/bin/patch --backup -p 0 /etc/postgresql/8.4/main/pg_hba.conf <<EOF
+--- pg_hba.conf.orig	2011-01-07 13:21:05.936789520 -0500
++++ pg_hba.conf	2011-01-07 13:31:12.880789053 -0500
+@@ -84,3 +84,4 @@
+ host    all         all         127.0.0.1/32          md5
+ # IPv6 local connections:
+ host    all         all         ::1/128               md5
++host    all         all         0.0.0.0/0             md5
+EOF
+check_errs $? "failed to patch pg_hba.conf"
+
+#----------------------------------------------------------------------
+# Restart PostgreSQL
+#----------------------------------------------------------------------
+/usr/sbin/service postgresql-8.4 restart
+check_errs $? "failed to restart postgresql-8.4"
+
+#----------------------------------------------------------------------
+# Create the portal user and db
+#----------------------------------------------------------------------
+sudo -u postgres /usr/bin/createuser -S -D -R portal
+check_errs $? "failed to create user portal"
+
+echo
+echo
+echo "@@@@@@@@@@@@@@@@@@@@@"
+echo "@"
+echo "@ Please enter a new database password for the 'portal' user"
+echo "@"
+sudo -u postgres /usr/bin/psql -c "\\password portal"
+check_errs $? "failed to set the password for portal user"
+
+sudo -u postgres /usr/bin/createdb portal
+check_errs $? "failed to create the portal database"
+
+#----------------------------------------------------------------------
+# All done.
+#----------------------------------------------------------------------
+exit 0
