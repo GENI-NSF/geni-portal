c318eac8c95eb9092600a1fbfddfe2076a8ab64f
Tom Mitchell <tmitchell@bbn.com>
2011-05-10 16:45:25 -0400

    Modularize and simplify.
    All inputs are generated in a single loop. Attributes that come from
    Shibboleth are marked as disabled so they are not passed to
    do-register. Do-register can pick up the values directly from the
    environment instead. Not really handling user inputted fields.

--- www/portal/register.php
+++ www/portal/register.php
@@ -2,46 +2,32 @@
 include("header.php");
 ?>
 
+<?php
+// Local functions
+function shib_input($shib_name, $pretty_name)
+{
+  print $pretty_name . ": ";
+  print "<input type=\"text\" name=\"$shib_name\"";
+  if (array_key_exists($shib_name, $_SERVER)) {
+    $value = $_SERVER[$shib_name];
+    echo "value=\"$value\" disabled=\"yes\"";
+  }
+  print "/><br/>";
+}
+?>
+
 <br/><br/>
 <h2> Registration Page </h2>
 <form method="POST" action="do-register.php">
-
-First name:
-<input type="text" name="firstname"
 <?php
-if (array_key_exists('givenName', $_SERVER)) {
-  $first = $_SERVER['givenName'];
-  echo "value=\"$first\" readonly=\"yes\"";
-}
+  shib_input('givenName', 'First name');
+  shib_input('sn', 'Last name');
+  shib_input('mail', 'EMail');
+  shib_input('telephoneNumber', 'Telephone');
 ?>
-/>
 <br/>
-
-Last name:
-<input type="text" name="lastname"
-<?php
-if (array_key_exists('sn', $_SERVER)) {
-  $last = $_SERVER['sn'];
-  echo "value=\"$last\" readonly=\"yes\"";
-}
-?>
-/>
-<br/>
-
-EMail:
-<input type="text" name="email"
-<?php
-if (array_key_exists('mail', $_SERVER)) {
-  $mail = $_SERVER['mail'];
-  echo "value=\"$mail\" readonly=\"yes\"";
-}
-?>
-/>
-<br/>
-
 <input type="submit" value="Register"/>
 </form>
-
 <?php
 include("footer.php");
 ?>
