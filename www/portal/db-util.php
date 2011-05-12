034f47c8f957fa98154dc2fb92b171780a694ed2
Tom Mitchell <tmitchell@bbn.com>
2011-05-12 13:23:46 -0400

    Load identity attributes.
    User load now includes idp attributes.

--- www/portal/db-util.php
+++ www/portal/db-util.php
@@ -26,17 +26,43 @@ function portal_conn()
 }
 
 function loadAccount($account_id) {
-  print "in db-util loadAccount<br/>";
+  /* print "in db-util loadAccount<br/>"; */
   $conn = portal_conn();
   $sql = "SELECT * FROM account WHERE account_id = "
   . $conn->quote($account_id, 'text')
   . ";";
-  print "Query = $sql<br/>";
+  /* print "Query = $sql<br/>"; */
   $resultset = $conn->query($sql);
   if (PEAR::isError($resultset)) {
-    die("error on identity id select: " . $resultset->getMessage());
+    die("error on loadAccount select: " . $resultset->getMessage());
   }
   $row = $resultset->fetchRow(MDB2_FETCHMODE_ASSOC);
+  // TODO: close the connection?
   return $row;
+}
+
+function loadIdentityAttributes($identity_id) {
+  $conn = portal_conn();
+  $sql = "SELECT * FROM identity_attribute WHERE identity_id = "
+  . $conn->quote($identity_id, 'integer')
+  . ";";
+  /* print "Query = $sql<br/>"; */
+  $resultset = $conn->query($sql);
+  if (PEAR::isError($resultset)) {
+    die("error on loadIdentityAttributes select: " . $resultset->getMessage());
+  }
+  //$rows = $resultset->fetchAll(MDB2_FETCHMODE_ASSOC);
+  $value = array();
+  while ($row = $resultset->fetchRow(MDB2_FETCHMODE_ASSOC)) {
+    // Append to the array
+    $value[] = $row;
+  }
+  // TODO: close the connection?
+    /* foreach ($value as $var => $value) { */
+    /*   print "geni_loadUser row $var = $value<br/>"; */
+    /* } */
+    /* print "returning $value<br/>"; */
+    /* print "num val attrs = " . count($value) . "<br/>"; */
+  return $value;
 }
 ?>
