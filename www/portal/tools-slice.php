00a18b613e48550fb1615e44ca118d5790d96d8b
Tom Mitchell <tmitchell@bbn.com>
2011-05-18 14:29:10 -0400

    Add display of slices.

--- www/portal/tools-slice.php
+++ www/portal/tools-slice.php
@@ -5,7 +5,25 @@ if (! $user->privSlice()) {
 }
 ?>
 <h1>Existing Slices</h1>
-<i>Table goes here</i><br/>
+<?php
+$slices = fetch_slices($user->account_id);
+if (count($slices) > 0) {
+  print "\n<table border=\"1\">\n";
+  print "<tr><th>Name</th><th>Expiration</th><th>Id</th></tr>\n";
+  foreach ($slices as $slice) {
+    print "<tr><td>"
+      . htmlentities($slice['name'])
+      . "</td><td>"
+      . htmlentities($slice['expiration'])
+      . "</td><td>"
+      . htmlentities($slice['slice_id'])
+      . "</td></tr>\n";
+  }
+  print "</table>\n";
+} else {
+  print "<i>No slices.</i><br/>\n";
+}
+?>
 <a href="
 <?php
 print relative_url("createslice");
