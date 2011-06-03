9f187721d519a5100e3f1a6edef5d6b602f54999
Tom Mitchell <tmitchell@bbn.com>
2011-06-03 13:56:19 -0400

    Add slice credential generation.
    Add link to slice table for downloading credential. Implement slice
    credential via slicecred.py script in gcf. Automatically downloaded
    via some HTTP header magic.

---
+++ www/portal/slicecred.php
@@ -0,0 +1,82 @@
+<?php
+//----------------------------------------------------------------------
+// Copyright (c) 2011 Raytheon BBN Technologies
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and/or hardware specification (the "Work") to
+// deal in the Work without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Work, and to permit persons to whom the Work
+// is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Work.
+//
+// THE WORK IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE WORK OR THE USE OR OTHER DEALINGS
+// IN THE WORK.
+//----------------------------------------------------------------------
+?>
+<?php
+//--------------------------------------------------
+// Globals - should go to settings
+//--------------------------------------------------
+$portal_gcf_dir = '/tmp/gcf';
+$portal_gcf_cfg_dir = '/tmp/gcf/portal';
+
+require_once("user.php");
+$user = geni_loadUser();
+if (! $user->privSlice()) {
+  exit();
+}
+?>
+<?php
+function no_slice_error() {
+  header('HTTP/1.1 404 Not Found');
+  print 'No slice id specified.';
+  exit();
+}
+
+if (! count($_GET)) {
+  // No parameters. Return an error result?
+  // For now, return nothing.
+  no_slice_error();
+}
+if (array_key_exists('id', $_GET)) {
+  $slice_id = $_GET['id'];
+} else {
+  no_slice_error();
+}
+// Look up the slice...
+$slice = fetch_slice($slice_id);
+
+// TODO: Does the $user have permissions on this $slice?
+// TODO: Pass expiration to slicecred.py
+
+// Run slicecred.py and return it as the content.
+$cmd_array = array($portal_gcf_dir . '/slicecred.py',
+                   $portal_gcf_cfg_dir . '/gcf.ini',
+                   $slice['name'],
+                   $portal_gcf_cfg_dir . '/ch-key.pem',
+                   $portal_gcf_cfg_dir . '/ch-cert.pem',
+                   // This should be the user's cert
+                   $portal_gcf_cfg_dir . '/alice-cert.pem'
+                   );
+$command = implode(" ", $cmd_array);
+$result = exec($command, $output, $status);
+//print_r($output);
+
+$file = $slice['name'] . "-cred.xml";
+// Set headers for download
+header("Cache-Control: public");
+header("Content-Description: File Transfer");
+header("Content-Disposition: attachment; filename=$file");
+header("Content-Type: text/xml");
+header("Content-Transfer-Encoding: binary");
+print implode("\n", $output);
+?>
--- www/portal/tools-slice.php
+++ www/portal/tools-slice.php
@@ -9,15 +9,19 @@ if (! $user->privSlice()) {
 $slices = fetch_slices($user->account_id);
 if (count($slices) > 0) {
   print "\n<table border=\"1\">\n";
-  print "<tr><th>Name</th><th>Expiration</th><th>Id</th></tr>\n";
+  print "<tr><th>Name</th><th>Expiration</th><th>Id</th><th>Credential</th></tr>\n";
+  $base_url = relative_url("slicecred.php?");
   foreach ($slices as $slice) {
-    print "<tr><td>"
-      . htmlentities($slice['name'])
-      . "</td><td>"
-      . htmlentities($slice['expiration'])
-      . "</td><td>"
-      . htmlentities($slice['slice_id'])
-      . "</td></tr>\n";
+    $slice_id = $slice['slice_id'];
+    $args['id'] = $slice_id;
+    $query = http_build_query($args);
+    $slicecred_url = $base_url . $query;
+    print "<tr>"
+      . "<td>" . htmlentities($slice['name']) . "</td>"
+      . "<td>" . htmlentities($slice['expiration']) . "</td>"
+      . "<td>" . htmlentities($slice_id) . "</td>"
+      . ("<td><a href=\"$slicecred_url\">Get Credential</a></td>")
+      . "</tr>\n";
   }
   print "</table>\n";
 } else {
