154c66cd29a64c9cf5eeade70f6bee96eb630206
Tom Mitchell <tmitchell@bbn.com>
2011-07-21 22:06:08 -0400

    Honor the auto-approve setting.
    If auto approve is enabled, the home page link goes to an approval
    script and then on to the home page. Otherwise it goes to the home
    page, which short circuits to a waiting-for-approval page.

--- www/portal/do-register.php
+++ www/portal/do-register.php
@@ -1,4 +1,28 @@
 <?php
+//----------------------------------------------------------------------
+// Copyright (c) 2011 Raytheon BBN Technologies
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and/or hardware specification (the "Work") to
+// deal in the Work without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Work, and to permit persons to whom the Work
+// is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Work.
+//
+// THE WORK IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE WORK OR THE USE OR OTHER DEALINGS
+// IN THE WORK.
+//----------------------------------------------------------------------
+?>
+<?php
 require_once("settings.php");
 require_once("db-util.php");
 require_once("util.php");
@@ -196,6 +220,12 @@ include("header.php");
 Go to the <a href=
 <?php
 $url = relative_url("home.php");
+if ($portal_auto_approve) {
+  $args['id'] = $account_id;
+  $query = http_build_query($args);
+  $url = relative_url("approve.php?");
+  $url = $url . $query;
+}
 print $url
 ?>
 >portal home page</a>
