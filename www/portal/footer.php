96e4d9207b6c63947b5c804941477707b6fbe1b1
Marshall Brinn <mbrinn@bbn.com>
2012-03-23 20:16:30 +0100

    Change 2011 to 2012 in footer

--- www/portal/footer.php
+++ www/portal/footer.php
@@ -2,7 +2,7 @@
 </div>
 <div id="footer">
     <hr/>
-    <small><i>GENI Portal Copyright 2011, BBN Technologies</i></small>
+    <small><i>GENI Portal Copyright 2012, BBN Technologies</i></small>
 </div>
 </body>
 </html>
