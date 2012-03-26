c763c16940a217ef76ee483bb23ebb90e8700a67
Tom Mitchell <tmitchell@bbn.com>
2012-03-26 10:32:08 -0400

    Fancy up the footer.
    Add links to GENI and the NSF.

--- www/portal/footer.php
+++ www/portal/footer.php
@@ -1,8 +1,12 @@
 <!-- close the "content" div. -->
 </div>
 <div id="footer">
-    <hr/>
-    <small><i>GENI Portal Copyright 2012, BBN Technologies</i></small>
+  <hr/>
+  <small><i><a href="http://www.geni.net/">GENI</a> is sponsored by the <a href="http://www.nsf.gov/">NSF</a></i></small>
+  <!-- put the copyright off to the right -->
+  <div style="float:right;">
+  <small><i>GENI Portal Copyright 2012, BBN Technologies</i></small>
+  </div>
 </div>
 </body>
 </html>
