60301b9cb7208c76245a4e591575f4f7f4fe6738
Marshall Brinn <mbrinn@bbn.com>
2012-03-30 14:47:56 -0400

    Indentation

--- lib/php/message_handler.php
+++ lib/php/message_handler.php
@@ -7,53 +7,55 @@
  * take the result and JSON encode into a document, encrypt and sign and return response
  */
 
+require_once("smime.php");
+
 function handle_message($prefix)
 {
-   error_log($prefix . ": starting");
-   $request_method = strtolower($_SERVER['REQUEST_METHOD']);
-   switch($request_method)
-     {
-     case 'put':
-       $putdata = fopen("php://input", "r");
-       $data = '';
-       error_log($prefix . " starting to read...");
-       while ($putchunk = fread($putdata, 1024))
-         {
-           error_log("Read chunk: $putchunk");
-           $data .= $putchunk;
-         }
-       fclose($putdata);
-       break;
-     case 'post':
-       if (array_key_exists('file', $_FILES)) {
-         $errorcode = $_FILES['file']['error'];
-         if ($errorcode != 0) {
-           // An error occurred with the upload.
-           if ($errorcode == UPLOAD_ERR_NO_FILE) {
-             $error = "No file was uploaded.";
-           } else {
-             $error = "Unknown upload error (code = $errorcode).";
-           }
-           error_log($prefix . ": $error");
-         } else {
-           $msg_file = $_FILES["file"]["tmp_name"];
-         }
-       }
-       break;
-     }
+  error_log($prefix . ": starting");
+  $request_method = strtolower($_SERVER['REQUEST_METHOD']);
+  switch($request_method)
+    {
+    case 'put':
+      $putdata = fopen("php://input", "r");
+      $data = '';
+      error_log($prefix . " starting to read...");
+      while ($putchunk = fread($putdata, 1024))
+	{
+	  error_log("Read chunk: $putchunk");
+	  $data .= $putchunk;
+	}
+      fclose($putdata);
+      break;
+    case 'post':
+      if (array_key_exists('file', $_FILES)) {
+	$errorcode = $_FILES['file']['error'];
+	if ($errorcode != 0) {
+	  // An error occurred with the upload.
+	  if ($errorcode == UPLOAD_ERR_NO_FILE) {
+	    $error = "No file was uploaded.";
+	  } else {
+	    $error = "Unknown upload error (code = $errorcode).";
+	  }
+	  error_log($prefix . ": $error");
+	} else {
+	  $msg_file = $_FILES["file"]["tmp_name"];
+	}
+      }
+      break;
+    }
    
-   error_log($prefix . ": finished switch");
-   // Now process the data
-   $data = smime_decrypt($data);
-   $msg = smime_validate($data);
-   // XXX Error check smime_validate result here
+  error_log($prefix . ": finished switch");
+  // Now process the data
+  $data = smime_decrypt($data);
+  $msg = smime_validate($data);
+  // XXX Error check smime_validate result here
    
-   $funcargs = parse_message($msg);
-   $result = call_user_func($funcargs[0], $funcargs[1]);
-   $output = encode_result($result);
-   $output = smime_sign_message($output);
-   $output = smime_encrypt($output);
-   print $output;
+  $funcargs = parse_message($msg);
+  $result = call_user_func($funcargs[0], $funcargs[1]);
+  $output = encode_result($result);
+  $output = smime_sign_message($output);
+  $output = smime_encrypt($output);
+  print $output;
 }
 
 ?>
