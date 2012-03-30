72edebd359018fd5400c02ad892ccbbbfdd89063
Marshall Brinn <mbrinn@bbn.com>
2012-03-30 10:50:01 -0400

    Add comments to lib sa _controller.

--- sa/www/sa_controller.php
+++ sa/www/sa_controller.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once("smime.php");
+require_once("message_handler.php");
 
 function create_slice($args)
 {
@@ -9,51 +10,6 @@ function create_slice($args)
   return "created $slice_name";
 }
 
-
-error_log("SA: starting");
-
-$request_method = strtolower($_SERVER['REQUEST_METHOD']);
-switch($request_method)
-  {
-  case 'put':
-    $putdata = fopen("php://input", "r");
-    $data = '';
-    error_log("SA starting to read...");
-    while ($putchunk = fread($putdata, 1024))
-      {
-        error_log("Read chunk: $putchunk");
-        $data .= $putchunk;
-      }
-    fclose($putdata);
-    break;
-  case 'post':
-    if (array_key_exists('file', $_FILES)) {
-      $errorcode = $_FILES['file']['error'];
-      if ($errorcode != 0) {
-        // An error occurred with the upload.
-        if ($errorcode == UPLOAD_ERR_NO_FILE) {
-          $error = "No file was uploaded.";
-        } else {
-          $error = "Unknown upload error (code = $errorcode).";
-        }
-        error_log("SA: $error");
-      } else {
-        $msg_file = $_FILES["file"]["tmp_name"];
-      }
-    }
-    break;
-  }
-error_log("SA: finished switch");
-// Now process the data
-$data = smime_decrypt($data);
-$msg = smime_validate($data);
-// XXX Error check smime_validate result here
-
-$funcargs = parse_message($msg);
-$result = call_user_func($funcargs[0], $funcargs[1]);
-$output = encode_result($result);
-$output = smime_sign_message($output);
-$output = smime_encrypt($output);
-print $output;
+handle_message("SA");
 
 ?>
