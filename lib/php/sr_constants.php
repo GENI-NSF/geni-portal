dc0d60fee179b340227ec4664943535303878a05
Marshall Brinn <mbrinn@bbn.com>
2012-03-31 05:36:05 -0400

    Refactor SR constants

---
+++ lib/php/sr_constants.php
@@ -0,0 +1,45 @@
+<?php
+
+/* Set of constants for managine calls and data records for 
+ * GENI Clearinghouse Service Registry
+ */
+
+/* Set of known services types for services within GENI CH SR */
+/* We store/retrieve by index into this array, but print the strings */
+$SR_SERVICE_TYPE_NAMES = array("AGGREGATE_MANAGER", 
+				      "SLICE_AUTHORITY", 
+				      "PROJECT_AUTHORITY", 
+				      "MEMBER_AUTHORITY",
+				      "AUTHORIZATION_SERVICE",
+				      "LOGGING_SERVICE",
+				      "CREDENTIAL_STORE");
+
+class SR_SERVICE_TYPE
+{
+  const AGGREGATE_MANAGER = 0;
+  const SLICE_AUTHORITY = 1;
+  const PROJECT_AUTHORITY = 2;
+  const MEMBER_AUTHORITY = 3;
+  const AUTHORIZATION_SERVICE = 4;
+  const LOGGING_SERVICE = 5;
+  const CREDENTIAL_STORE = 6;
+}
+
+/* Set of arguments in calls to the SR interface */
+class SR_ARGUMENT
+{
+  const SERVICE_URL = "service_url";
+  const SERVICE_TYPE = "service_type";
+}
+
+/* Name of table to which the SR persists/retrieves model state */
+$SR_TABLENAME = "service_registry";
+
+/* SR table has the following fields */
+class SR_TABLE_FIELDNAME {
+  const SERVICE_TYPE = "service_type";
+  const SERVICE_URL = "service_url";
+  const SERVICE_CERT = "service_cert";
+}
+
+?>
