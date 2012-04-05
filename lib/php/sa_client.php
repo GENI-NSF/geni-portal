fb8fce0b68bac518bd742d204e3b173e1d5c44d2
Tom Mitchell <tmitchell@bbn.com>
2012-04-05 13:02:45 -0400

    implement sa client get_slice_credential()

--- lib/php/sa_client.php
+++ lib/php/sa_client.php
@@ -4,7 +4,13 @@
 
 function get_slice_credential($sa_url, $slice_id, $user)
 {
-  // *** WRITE ME
-  return null;
+  $row = db_fetch_inside_private_key_cert($user->account_id);
+  $pretty_row = print_r($row, true);
+  error_log("SAClient: row = $pretty_row");
+  $cert = $row['certificate'];
+  $message['operation'] = 'create_slice_credential';
+  $message['slice_name'] = $slice_id;
+  $message['experimenter_certificate'] = $cert;
+  $result = put_message($sa_url, $message);
+  return $result['slice_credential'];
 }
-
