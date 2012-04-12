f6d692dd608d91b707ad1d10c6225f8b3443b9f8
Marshall Brinn <mbrinn@bbn.com>
2012-04-12 11:01:02 -0400

    remove dummy lines

--- sr/db/postgresql/data.sql
+++ sr/db/postgresql/data.sql
@@ -2,7 +2,7 @@
 -- ----------------------------------------------------------------------
 -- A few fake records to insert into the database
 -- ----------------------------------------------------------------------
-insert into service_registry (service_type, service_url) values (0, 'http://foo.bar/AM1');
-insert into service_registry (service_type, service_url) values (0, 'http://foo.bar/AM2');
-insert into service_registry (service_type, service_url) values (1, 'http://foo.bar/SA');
-insert into service_registry (service_type, service_url) values (2, 'http://foo.bar/PA');
+--insert into service_registry (service_type, service_url) values (0, 'http://foo.bar/AM1');
+--insert into service_registry (service_type, service_url) values (0, 'http://foo.bar/AM2');
+--insert into service_registry (service_type, service_url) values (1, 'http://foo.bar/SA');
+--insert into service_registry (service_type, service_url) values (2, 'http://foo.bar/PA');
