ed5f469169a88fc0b6f81be097cd77c67cdac758
Aaron Helsinger <ahelsing@bbn.com>
2012-04-13 10:20:55 -0400

    Organize as a table; make member list show the slice owner; comment out debug printouts; fix up the renew form

--- portal/www/portal/slice.php
+++ portal/www/portal/slice.php
@@ -42,8 +42,8 @@ $slice = "<None>";
 if (array_key_exists("id", $_GET)) {
   $slice = $_GET['id'];
   $slice_item = lookup_slice($sa_url, $slice);
-  $pretty_result = print_r($slice_item, true);
-  error_log("fetch_slice result: $pretty_result\n");
+  //$pretty_result = print_r($slice_item, true);
+  //error_log("fetch_slice result: $pretty_result\n");
 
   $name = $slice_item[SA_ARGUMENT::SLICE_NAME];
   $slice_expiration = $slice_item[SA_ARGUMENT::EXPIRATION];
@@ -54,12 +54,12 @@ if (array_key_exists("id", $_GET)) {
   $owner_email = $owner->email();
 
   $slice_project_id = $slice_item[SA_ARGUMENT::PROJECT_ID];
-  error_log("slice_project_id result: $slice_project_id\n");
+  //error_log("slice_project_id result: $slice_project_id\n");
   $project_details = lookup_project($pa_url, $slice_project_id);
-  error_log("pa_url result: $pa_url\n");
-  error_log("project_details result: $project_details\n");
+  //error_log("pa_url result: $pa_url\n");
+  //error_log("project_details result: $project_details\n");
   $slice_project_name = $project_details[PA_PROJECT_TABLE_FIELDNAME::PROJECT_NAME];
-  error_log("slice_project_name result: $slice_project_name\n");
+  //error_log("slice_project_name result: $slice_project_name\n");
 }
 
 $edit_url = 'edit-slice.php?id='.$slice;
@@ -67,22 +67,26 @@ $add_url = 'slice-add-resources.php?id='.$slice;
 $res_url = 'sliceresource.php?id='.$slice;
 $proj_url = 'project.php?id='.$slice_project_id;
 print "<h1>GENI Slice: " . $name ." </h1>\n";
-print "<b>Name</b>: $name<br/>\n";
-print "<b>Member of Project</b>: <a href=$proj_url>$slice_project_name</a><br/>\n";
-print "<b>Warning: Slice name is public</b><br/>\n";
-print "<b>Slice URN</b>: $slice_urn<br/>\n";
-print "<b>Slice UUID</b>: $slice<br/>\n";
-print "<b>Slice Owner</b>: $slice_owner_name <a href='mailto:$owner_email'>e-mail</a><br/>\n";
-print "<b>Slice Expiration</b>: $slice_expiration<br/>\n";
-print "<b>Other static info</b>: etc<br/>";
+print "<table><tr><th>Name</th><th>Value</th></tr>\n";
+print "<tr><td><b>Name</b></td><td>$name</td></tr>\n";
+print "<tr><td><b>Member of Project</b></td><td><a href=$proj_url>$slice_project_name</a></td></tr>\n";
+print "<tr><td><b>Slice URN</b></td><td>$slice_urn</td></tr>\n";
+print "<tr><td><b>Slice UUID</b></td><td>$slice</td></tr>\n";
+print "<tr><td><b>Slice Owner</b></td><td>$slice_owner_name <a href='mailto:$owner_email'>e-mail</a></td></tr>\n";
+print "<tr><td><b>Slice Expiration</b></td><td>$slice_expiration</td></tr>\n";
+print "</table>\n";
+print "<br/>\n";
 print "<a href=$edit_url>Edit Slice</a>";
 ?>
 
 <br/>
-<b>Date to renew until</b>:  <br/>
 <form method='POST' action='do-renew.php'>
-<input type='text' name='Renew'/>
-<input type='submit' value='Renew'/>
+<b>Date to renew until</b>: <input type='text' name='Renew' 
+<?php
+  print "value=\"$slice_expiration\"/><br/>\n";
+  print "<input type=\"hidden\" name=\"id\" value=\"$slice\"/><br/>\n";
+?>
+<input type='submit' name= 'Renew' value='Renew'/>
 </form>
 
 <br/>
@@ -98,15 +102,12 @@ if ($user->privAdmin()) {
 }
 ?>
 
-
-
-
 <h2>Slice members</h2>
 <table border="1">
 <tr><th>Slice Member</th><th>Roles</th></tr>
 <?php
    // FIXME: See project-member.php. Replace all that with a table or 2 here?
-   print "<tr><td><a href=\"project-member.php?id=" . $slice . "&member=joe\">Joe</a></td><td>Lead</td></tr>\n";
+   print "<tr><td><a href=\"slice-member.php?id=" . $slice . "&member=$slice_owner_id\">$slice_owner_name</a></td><td>Owner</td></tr>\n";
 ?>
 </table>
 
