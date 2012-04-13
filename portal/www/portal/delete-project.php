6dd014803348f44a56780bb9ff9b740fcde537be
Aaron Helsinger <ahelsing@bbn.com>
2012-04-13 13:58:52 -0400

    deleting projects works

--- portal/www/portal/delete-project.php
+++ portal/www/portal/delete-project.php
@@ -24,6 +24,10 @@
 
 require_once("user.php");
 require_once("header.php");
+require_once('sr_constants.php');
+require_once('sr_client.php');
+require_once('pa_constants.php');
+require_once('pa_client.php');
 show_header('GENI Portal: Projects', $TAB_PROJECTS);
 $user = geni_loadUser();
 if (!isset($user) || is_null($user) || ! $user->isActive()) {
