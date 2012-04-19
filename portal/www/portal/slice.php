dc6f613340aa981afa3cc8bda302cb59bacea81e
Marshall Brinn <mbrinn@bbn.com>
2012-04-19 17:09:25 -0400

    require logging_client.php

--- portal/www/portal/slice.php
+++ portal/www/portal/slice.php
@@ -31,6 +31,8 @@ require_once('sr_constants.php');
 require_once('sr_client.php');
 require_once("sa_constants.php");
 require_once("sa_client.php");
+require_once('logging_client.php');
+
 $user = geni_loadUser();
 if (!isset($user) || is_null($user) || ! $user->isActive()) {
   relative_redirect('home.php');
