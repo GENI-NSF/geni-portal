9ff7a97211172fa16e5426455a88377d261b3132
Tom Mitchell <tmitchell@bbn.com>
2012-04-24 17:14:13 -0400

    Add a few more sample rspecs.
    They all point to an rspec requesting a single node, but they have
    different (and misleading) names and descriptions.

--- portal/db/postgresql/data.sql
+++ portal/db/postgresql/data.sql
@@ -3,7 +3,44 @@ INSERT INTO schema_version(key, extra) values ('003', 'schema version');
 -- Add a request rspec for a single node.
 INSERT INTO rspec(name, schema, schema_version, description, rspec)
   VALUES
-  ('One compute node', 'GENI', '3', 'Any one compute node.',
+  ('One compute node', 'GENI', '3',
+   'Any one compute node.',
+   '<?xml version="1.0" encoding="UTF-8"?>
+<rspec xmlns="http://www.geni.net/resources/rspec/3"
+       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+       xsi:schemaLocation="http://www.geni.net/resources/rspec/3 http://www.geni.net/resources/rspec/3/request.xsd"
+       type="request">
+  <node client_id="foo"/>
+</rspec>'
+);
+INSERT INTO rspec(name, schema, schema_version, description, rspec)
+  VALUES
+  ('Two compute nodes', 'GENI', '3',
+   'Any two compute nodes.',
+   '<?xml version="1.0" encoding="UTF-8"?>
+<rspec xmlns="http://www.geni.net/resources/rspec/3"
+       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+       xsi:schemaLocation="http://www.geni.net/resources/rspec/3 http://www.geni.net/resources/rspec/3/request.xsd"
+       type="request">
+  <node client_id="foo"/>
+</rspec>'
+);
+INSERT INTO rspec(name, schema, schema_version, description, rspec)
+  VALUES
+  ('Two nodes, one link', 'GENI', '3',
+   'Two nodes with a link between them.',
+   '<?xml version="1.0" encoding="UTF-8"?>
+<rspec xmlns="http://www.geni.net/resources/rspec/3"
+       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+       xsi:schemaLocation="http://www.geni.net/resources/rspec/3 http://www.geni.net/resources/rspec/3/request.xsd"
+       type="request">
+  <node client_id="foo"/>
+</rspec>'
+);
+INSERT INTO rspec(name, schema, schema_version, description, rspec)
+  VALUES
+  ('Three nodes, triangle topology', 'GENI', '3',
+   'Three nodes in a triangle topology.',
    '<?xml version="1.0" encoding="UTF-8"?>
 <rspec xmlns="http://www.geni.net/resources/rspec/3"
        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
