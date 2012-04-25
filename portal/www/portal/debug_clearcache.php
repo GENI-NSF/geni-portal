d11c0492e10ca8574efe7be47fb316d988c3277d
Tom Mitchell <tmitchell@bbn.com>
2012-04-25 10:46:57 -0400

    Add cache clearing to debug page.

--- portal/www/portal/debug_clearcache.php
+++ portal/www/portal/debug_clearcache.php
@@ -22,62 +22,35 @@
 // IN THE WORK.
 //----------------------------------------------------------------------
 
+/**
+ * Clear the cache. Remove all keys from the _SESSION array.
+ */
+
+
+$cleared_keys = array();
+session_start();
+if (isset($_SESSION)) {
+  foreach (array_keys($_SESSION) as $skey) {
+    $cleared_keys[] = $skey;
+    unset($_SESSION[$skey]);
+  }
+}
+
 require_once("user.php");
 require_once("header.php");
-show_header('GENI Portal: Debug', $TAB_DEBUG);
-?>
-<div id="debug-body">
-<?php
-$user = geni_loadUser();
-?>
-<h2>DB</h2>
-<?php
-print "<a href=\"db_error_test.php\">Database Error Test</a>\n";
-?>
-<h2>LOG</h2>
-<?php
-print "<a href=\"logging_test.php\">Logging Test</a>\n";
-?>
-<h2>SR</h2>
-<?php
-print "<a href=\"sr_controller_test.php\">Service Registry Test</a>\n";
-?>
-<h2>AuthZ</h2>
-<?php
-print "<a href=\"permission_manager_test.php\">Permission Manager Test</a>\n";
-?>
-<h2>PA</h2>
-<?php
-print "<a href=\"pa_controller_test.php\">Project Authority Test</a>\n";
-?>
-<h2>SA</h2>
-<?php
-print "<a href=\"sa_controller_test.php\">Slice Authority Test</a>\n";
-?>
-<h2>MA</h2>
-<?php
-print "<a href=\"ma_controller_test.php\">Member Authority Test</a>\n";
-?>
-<h2>CS</h2>
-<?php
-print "<a href=\"cs_controller_test.php\">Credential Store Test</a>\n";
-?>
+show_header('GENI Portal: Clear cache', $TAB_DEBUG);
 
-<h2>AM</h2>
-<?php
-print "<a href=\"getversion.php\">GetVersion on AM</a>\n";
-?>
-<br/>
-<?php
-print "<a href=\"listresources_plain.php\">ListResources on AM (no slice)</a>\n";
-?>
-<h2>Cache</h2>
-<?php
-print "<a href=\"debug_clearcache.php\">Clear cache</a>\n";
-?>
+if (count($cleared_keys) > 0) {
+  print "<h1>Cache Cleared</h1>\n";
+  print "The following session keys have been unset:\n";
+  print "<ul>\n";
+  foreach ($cleared_keys as $key) {
+    print "<li>$key</li>\n";
+  }
+  print "</ul>\n";
+} else {
+  print "The cache contained no values.\n";
+}
 
-</div><!-- debug-body -->
-<br/>
-<?php
 include("footer.php");
 ?>
