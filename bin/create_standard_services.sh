842a01e07849464478e6cf89399ad69d4f835620
Tom Mitchell <tmitchell@bbn.com>
2012-04-30 14:45:14 -0400

    Begin real CA.

--- bin/create_standard_services.sh
+++ bin/create_standard_services.sh
@@ -3,16 +3,52 @@
 FILENAME="/tmp/standard_services.$USER"
 #echo $FILENAME
 
-echo "insert into service_registry (service_type, service_url) values (1, 'https://$HOSTNAME/sa/sa_controller.php');" > $FILENAME
-echo "insert into service_registry (service_type, service_url) values (2, 'https://$HOSTNAME/pa/pa_controller.php');" >> $FILENAME
-echo "insert into service_registry (service_type, service_url) values (3, 'https://$HOSTNAME/ma/ma_controller.php');" >> $FILENAME
-echo "insert into service_registry (service_type, service_url) values (5, 'https://$HOSTNAME/logging/logging_controller.php');" >> $FILENAME
-echo "insert into service_registry (service_type, service_url) values (6, 'https://$HOSTNAME/cs/cs_controller.php');" >> $FILENAME
+BASEDIR=/usr/share/geni-ch
+
+# SR
+./mk-auth-req sr "${BASEDIR}/sr/sr-key.pem" "${BASEDIR}/sr/sr-req.pem"
+./sign-auth-req "${BASEDIR}/sr/sr-req.pem" "${BASEDIR}/sr/sr-cert.pem"
+
+# SA
+./mk-auth-req sa "${BASEDIR}/sa/sa-key.pem" "${BASEDIR}/sa/sa-req.pem"
+./sign-auth-req "${BASEDIR}/sa/sa-req.pem" "${BASEDIR}/sa/sa-cert.pem" CA
+CERT=`openssl x509 -in "${BASEDIR}/sa/sa-cert.pem"`
+echo "insert into service_registry (service_type, service_url, service_cert) values (1, 'https://$HOSTNAME/sa/sa_controller.php', '${CERT}');" > $FILENAME
+
+
+# PA
+./mk-auth-req pa "${BASEDIR}/pa/pa-key.pem" "${BASEDIR}/pa/pa-req.pem"
+./sign-auth-req "${BASEDIR}/pa/pa-req.pem" "${BASEDIR}/pa/pa-cert.pem"
+CERT=`openssl x509 -in "${BASEDIR}/pa/pa-cert.pem"`
+echo "insert into service_registry (service_type, service_url, service_cert) values (2, 'https://$HOSTNAME/pa/pa_controller.php', '${CERT}');" >> $FILENAME
+
+
+# MA
+./mk-auth-req ma "${BASEDIR}/ma/ma-key.pem" "${BASEDIR}/ma/ma-req.pem"
+./sign-auth-req "${BASEDIR}/ma/ma-req.pem" "${BASEDIR}/ma/ma-cert.pem" CA
+CERT=`openssl x509 -in "${BASEDIR}/ma/ma-cert.pem"`
+echo "insert into service_registry (service_type, service_url, service_cert) values (3, 'https://$HOSTNAME/ma/ma_controller.php', '${CERT}');" >> $FILENAME
+
+
+# LOGGING
+./mk-auth-req logging "${BASEDIR}/logging/logging-key.pem" "${BASEDIR}/logging/logging-req.pem"
+./sign-auth-req "${BASEDIR}/logging/logging-req.pem" "${BASEDIR}/logging/logging-cert.pem"
+CERT=`openssl x509 -in "${BASEDIR}/logging/logging-cert.pem"`
+echo "insert into service_registry (service_type, service_url, service_cert) values (5, 'https://$HOSTNAME/logging/logging_controller.php', '${CERT}');" >> $FILENAME
+
+
+# CS
+./mk-auth-req cs "${BASEDIR}/cs/cs-key.pem" "${BASEDIR}/cs/cs-req.pem"
+./sign-auth-req "${BASEDIR}/cs/cs-req.pem" "${BASEDIR}/cs/cs-cert.pem"
+CERT=`openssl x509 -in "${BASEDIR}/cs/cs-cert.pem"`
+echo "insert into service_registry (service_type, service_url, service_cert) values (6, 'https://$HOSTNAME/cs/cs_controller.php', '${CERT}');" >> $FILENAME
+
 
 # A local aggregate manager
 echo "insert into service_registry (service_type, service_url, service_name, service_description) values (0, 'https://localhost:8001/', 'Local gcf AM', 'Empty AM');" >> $FILENAME
 
-psql -U portal -h localhost portal < $FILENAME
+# Write the accumulated SQL to the database
+sudo -u $SUDO_USER psql -U portal -h localhost portal < $FILENAME
 
+# Delete the temp file
 rm $FILENAME
-
