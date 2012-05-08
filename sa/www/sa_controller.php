1a81228cc6ae16bb4e6a9ed96b41eb9087c65ecf
Tom Mitchell <tmitchell@bbn.com>
2012-05-08 16:56:19 -0400

    Use the message signer uuid in lookup_slices.
    Begin using the signed message capabilities to dynamically determine
    the caller. Use that caller to query for slices. Also quote owner_id
    and project_id properly for the database query.

--- sa/www/sa_controller.php
+++ sa/www/sa_controller.php
@@ -270,21 +270,27 @@ function lookup_slices($args, $message)
 {
   global $SA_SLICE_TABLENAME;
 
-  error_log("SA.loookup_slices: \$message->signer = "
-            . print_r($message->signer(), true));
-  $signer_urn = $message->signerUrn();
-  error_log("SA.lookup_slices signer urn = $signer_urn");
-
   $project_id = $args[SA_ARGUMENT::PROJECT_ID];
-  $project_id_clause = '';
-  if ($project_id <> null) {
-    $project_id_clause = SA_SLICE_TABLE_FIELDNAME::PROJECT_ID . " = '" . $project_id . "'";
+  $owner_id = $message->signerUuid();
+
+  if (is_null($owner_id)) {
+    return generate_response(RESPONSE_ERROR::ARGS,
+                             NULL,
+                             "No message signer UUID available.");
   }
 
-  $owner_id = $args[SA_ARGUMENT::OWNER_ID];
+  $conn = db_conn();
+
+  $project_id_clause = '';
+  if ($project_id <> null) {
+    $project_id_clause = SA_SLICE_TABLE_FIELDNAME::PROJECT_ID
+      . " = " . $conn->quote($project_id, 'text');
+  }
+
   $owner_id_clause = '';
   if ($owner_id <> null) {
-    $owner_id_clause = SA_SLICE_TABLE_FIELDNAME::OWNER_ID . " = '" . $owner_id . "'";
+    $owner_id_clause = SA_SLICE_TABLE_FIELDNAME::OWNER_ID
+      . " = " . $conn->quote($owner_id, 'text');
   }
 
   $where_clause = "";
