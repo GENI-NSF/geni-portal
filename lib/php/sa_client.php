ed95bfbb188a3f6e7b732d94c190ae6db444dbc6
Tom Mitchell <tmitchell@bbn.com>
2012-05-09 13:40:00 -0400

    Require a signer on lookup_slices.
    This is the first service api to use message signing.

--- lib/php/sa_client.php
+++ lib/php/sa_client.php
@@ -105,12 +105,15 @@ function lookup_slices_by_project_and_name($sa_url, $project_id, $slice_name)
 }
 
 /* lookup a set of slices by name, project_id, owner */
-function lookup_slices($sa_url, $project_id, $owner_id)
+function lookup_slices($sa_url, $signer, $project_id, $owner_id)
 {
+  $signer_cert = $signer->certificate();
+  $signer_key = $signer->privateKey();
   $lookup_slices_message['operation'] = 'lookup_slices';
   $lookup_slices_message[SA_ARGUMENT::PROJECT_ID] = $project_id;
   $lookup_slices_message[SA_ARGUMENT::OWNER_ID] = $owner_id;
-  $slices = put_message($sa_url, $lookup_slices_message);
+  $slices = put_message($sa_url, $lookup_slices_message, $signer_cert,
+                        $signer_key);
   return $slices;
 }
 
