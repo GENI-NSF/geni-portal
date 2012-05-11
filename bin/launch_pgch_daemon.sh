fcbb4b941672c833d8139062a339b494a773be6c
Marshall Brinn <mbrinn@bbn.com>
2012-05-11 11:03:26 -0400

    Set gcf.ini to /usr/share/geni-ch/portal/gcf.d/gcf.ini

--- bin/launch_pgch_daemon.sh
+++ bin/launch_pgch_daemon.sh
@@ -2,6 +2,6 @@
 # Must be run as root
 cd /usr/share/geni-ch/portal/gcf
 pkill -f -u root "python .*gcf-pgch"
-python ./src/gcf-pgch.py -c gcf.ini >/var/log/gcf-pgch.log 2>&1 &
+python ./src/gcf-pgch.py -c /usr/share/geni-ch/portal/gcf.d/gcf.ini >/var/log/gcf-pgch.log 2>&1 &
 
 
