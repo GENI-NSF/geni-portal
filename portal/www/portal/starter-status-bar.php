fb7a40317dcf917debc9ccda1f37079112e2eee2
Aaron Helsinger <ahelsing@bbn.com>
2012-06-05 16:28:39 -0400

    verbify nav bar and add copyright

--- portal/www/portal/starter-status-bar.php
+++ portal/www/portal/starter-status-bar.php
@@ -1,4 +1,26 @@
 <?php
+//----------------------------------------------------------------------
+// Copyright (c) 2012 Raytheon BBN Technologies
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and/or hardware specification (the "Work") to
+// deal in the Work without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Work, and to permit persons to whom the Work
+// is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Work.
+//
+// THE WORK IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE WORK OR THE USE OR OTHER DEALINGS
+// IN THE WORK.
+//----------------------------------------------------------------------
 
 require_once('util.php');
 require_once('sa_client.php');
@@ -103,11 +125,11 @@ function show_starter_status_bar($load_user)
 
   echo "<p class='starter'>";
   echo "GENI START:   ";
-  put_starter_status($activated, "ACTIVE", "home.php");
-  put_starter_status(count($ssh_keys)> 0, "SSH_KEYS", "profile.php");
+  put_starter_status($activated, "Activate Account", "home.php");
+  put_starter_status(count($projects)> 0, "Join Project", "projects.php");
+  put_starter_status(count($ssh_keys)> 0, "Register SSH Keys", "profile.php");
   //   put_starter_status(count($project_requests)> 0, "REQUESTS");
-  put_starter_status(count($projects)> 0, "PROJECTS", "projects.php");
-  put_starter_status(count($slices)> 0, "SLICES", "projects.php");
+  put_starter_status(count($slices)> 0, "Join Slice", "projects.php");
   echo "</p>";
 }
 
