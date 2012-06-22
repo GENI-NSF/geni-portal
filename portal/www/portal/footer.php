73a030587575044d511029d404c9e095b1d94567
Tom Mitchell <tmitchell@bbn.com>
2012-06-22 17:11:39 -0400

    Remove copyright from footer.

--- portal/www/portal/footer.php
+++ portal/www/portal/footer.php
@@ -28,10 +28,6 @@
 <hr/>
 <div id="footer">
   <small><i><a href="http://www.geni.net/">GENI</a> is sponsored by the <a href="http://www.nsf.gov/">NSF</a></i></small>
-  <!-- put the copyright off to the right -->
-  <div style="float:right;">
-  <small><i>GENI Portal Copyright 2012, BBN Technologies</i></small>
-  </div>
 </div>
 </body>
 </html>
