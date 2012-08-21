fc7d103acece673f0fd62658f0e6322296a1c5d5
Tom Mitchell <tmitchell@bbn.com>
2012-08-21 18:23:00 -0400

    Remove the obsolete MA_MEMBER table.

--- ma/db/postgresql/schema.sql
+++ ma/db/postgresql/schema.sql
@@ -5,18 +5,5 @@ set client_min_messages='WARNING';
 -- ----------------------------------------------------------------------
 --
 -- ----------------------------------------------------------------------
--- Drop the data first, then the type.
+-- Drop an obsolete table if it exists.
 DROP TABLE IF EXISTS ma_member;
-
-
-CREATE TABLE ma_member (
-  id SERIAL,
-  member_id UUID,
-  role_type int,
-  context_type int,
-  context_id UUID,
-  PRIMARY KEY (id)
-);
-
--- A common query, but so far the DB Doesn't use this.
--- CREATE INDEX ma_member_index_member ON ma_member (member_id);
