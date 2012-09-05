219e924c5dfdb655b7a3ac055717eb28cefbd438
Tom Mitchell <tmitchell@bbn.com>
2012-09-05 14:56:55 -0400

    Move non-API MA funcs to ma_utils.

--- ma/www/ma_controller.php
+++ ma/www/ma_controller.php
@@ -135,131 +135,9 @@ function lookup_keys_and_certs($args, $message)
 }
 
 
-function attr_key_exists($key, $attrs) {
-  foreach ($attrs as $attr) {
-    if ($attr[MA_ATTRIBUTE::NAME] === $key
-            && (! empty($attr[MA_ATTRIBUTE::VALUE]))) {
-      return TRUE;
-    }
-  }
-  return FALSE;
-}
-
 
 /**
- * Verify that all keys in $keys exist in $search.
  *
- * @param unknown_type $search
- * @param unknown_type $keys
- * @param unknown_type $missing
- * @return TRUE if all keys are in $search, FALSE otherwise.
- */
-function verify_keys($search, $keys, &$missing)
-{
-  $missing = array();
-  foreach ($keys as $key) {
-    if (! attr_key_exists($key, $search)) {
-      $missing[] = $key;
-    }
-  }
-  return $missing ? FALSE : TRUE;
-}
-
-function assert_project_lead($cs_url, $ma_signer, $member_id)
-{
-  $signer = NULL; /* this feels wrong */
-  $attribute = CS_ATTRIBUTE_TYPE::LEAD;
-  $context_type = CS_CONTEXT_TYPE::RESOURCE;
-  $context = NULL;
-  $result = create_assertion($cs_url, $ma_signer, $signer, $member_id,
-          $attribute, $context_type, $context);
-  geni_syslog(GENI_SYSLOG_PREFIX::MA,
-          "assert_project_lead got result " . print_r($result, TRUE));
-  return TRUE;
-}
-
-/**
- * Determine if a username already exists.
- * @param unknown_type $username
- */
-function username_exists($username) {
-  global $MA_MEMBER_ATTRIBUTE_TABLENAME;
-  $conn = db_conn();
-  $sql = ("select " . MA_MEMBER_ATTRIBUTE_TABLE_FIELDNAME::VALUE
-          . " from " . $MA_MEMBER_ATTRIBUTE_TABLENAME
-          . " where " . MA_MEMBER_ATTRIBUTE_TABLE_FIELDNAME::NAME
-          . " = " . $conn->quote(MA_ATTRIBUTE_NAME::USERNAME, 'text')
-          . " and " . MA_MEMBER_ATTRIBUTE_TABLE_FIELDNAME::VALUE
-          . " = " . $conn->quote($username, 'text'));
-  $result = db_fetch_rows($sql);
-  if ($result[RESPONSE_ARGUMENT::CODE] != RESPONSE_ERROR::NONE) {
-    $db_error = $result[RESPONSE_ARGUMENT::OUTPUT];
-    geni_syslog(GENI_SYSLOG_PREFIX::MA,
-            ("Database error: $db_error"));
-    geni_syslog(GENI_SYSLOG_PREFIX::MA, "Query was: " . $sql);
-    throw new ErrorException("Database failure: $db_error");
-  }
-  // True if an existing username was found, false otherwise.
-  return count($result[RESPONSE_ARGUMENT::VALUE]) != 0;
-}
-
-function derive_username($email_address) {
-  // See http://www.linuxjournal.com/article/9585
-  // try to figure out a reasonable username.
-  $email_addr = filter_var($email_address, FILTER_SANITIZE_EMAIL);
-  /* print "<br/>derive2: email_addr = $email_addr<br/>\n"; */
-
-  // Now get the username portion.
-  $atindex = strrpos($email_addr, "@");
-  /* print "atindex = $atindex<br/>\n"; */
-  $username = substr($email_addr, 0, $atindex);
-  /* print "base username = $username<br/>\n"; */
-
-  // FIXME: Follow the rules here: http://groups.geni.net/geni/wiki/GeniApiIdentifiers#Name
-  // Max 8 characters
-  // Case insensitive internally
-  // Obey this regex: '^[a-zA-Z][\w]\{1,8\}$'
-
-  // Sanitize the username so it can be used in ABAC
-  $username = strtolower($username);
-  $username = preg_replace("/[^a-z0-9_]/", "", $username);
-  if (! username_exists($username)) {
-    /* print "no conflict with $username<br/>\n"; */
-    return $username;
-  } else {
-    for ($i = 1; $i <= 99; $i++) {
-      $tmpname = $username . $i;
-      /* print "trying $tmpname<br/>\n"; */
-      if (! username_exists($tmpname)) {
-        /* print "no conflict with $tmpname<br/>\n"; */
-        return $tmpname;
-      }
-    }
-  }
-  throw new Exception("Unable to find a username based on $username");
-}
-
-function make_member_urn($ma_signer, $username)
-{
-  $ma_urn = urn_from_cert($ma_signer->certificate());
-  parse_urn($ma_urn, $ma_authority, $ma_type, $ma_name);
-  return make_urn($ma_authority, 'user', $username);
-}
-
-function make_member_urn_attribute($ma_signer, $username)
-{
-  $urn = make_member_urn($ma_signer, $username);
-  return array(MA_ATTRIBUTE::NAME => MA_ATTRIBUTE_NAME::URN,
-          MA_ATTRIBUTE::VALUE => $urn,
-          MA_ATTRIBUTE::SELF_ASSERTED => false);
-}
-
-/**
- * Return new member id? Return full member record?
- *
- * @param unknown_type $args
- * @param unknown_type $message
- * @return NULL|unknown
  */
 function create_account($args, $message)
 {
@@ -413,28 +291,6 @@ function ma_list_authorized_clients($args, $message)
   return generate_response(RESPONSE_ERROR::NONE, $result, '');
 }
 
-function make_inside_cert_key($member_id, $client_urn, $signer_cert_file,
-        $signer_key_file, &$cert, &$key) {
-  // Not using client urn yet
-  $cert = NULL;
-  $key = NULL;
-  $email = NULL;
-  $urn = NULL;
-  $info = get_member_info($member_id);
-  foreach ($info[MA_ARGUMENT::ATTRIBUTES] as $attr) {
-    if ($attr[MA_ATTRIBUTE::NAME] === MA_ATTRIBUTE_NAME::EMAIL_ADDRESS) {
-      $email = $attr[MA_ATTRIBUTE::VALUE];
-    } elseif ($attr[MA_ATTRIBUTE::NAME] === MA_ATTRIBUTE_NAME::URN) {
-      $urn = $attr[MA_ATTRIBUTE::VALUE];
-    }
-  }
-  make_cert_and_key($member_id, $email, $urn,
-          $signer_cert_file, $signer_key_file,
-          &$cert, &$key);
-  return true;
-}
-
-
 /**
  * Authorize (or deauthorize) a given tool for a given user
  * if 'authorize_sense' is true, authorize (add line to ma_inside_key table)
@@ -478,36 +334,6 @@ function ma_authorize_client($args, $message)
     $result = db_execute_statement($sql);
   }
 
-  return $result;
-}
-
-/* NOTE: This is an internal function and not part of the MA API function. */
-function get_member_info($member_id)
-{
-  global $MA_MEMBER_ATTRIBUTE_TABLENAME;
-  $conn = db_conn();
-  $sql = "select " . MA_MEMBER_ATTRIBUTE_TABLE_FIELDNAME::NAME
-  . ", " . MA_MEMBER_ATTRIBUTE_TABLE_FIELDNAME::VALUE
-  . ", " . MA_MEMBER_ATTRIBUTE_TABLE_FIELDNAME::SELF_ASSERTED
-  . " from " . $MA_MEMBER_ATTRIBUTE_TABLENAME
-  . " where " . MA_MEMBER_ATTRIBUTE_TABLE_FIELDNAME::MEMBER_ID
-  . " = " . $conn->quote($member_id, 'text');
-  $rows = db_fetch_rows($sql);
-  // Convert $rows to an array of member_ids
-  $attrs = array();
-  foreach ($rows[RESPONSE_ARGUMENT::VALUE] as $row) {
-    $aname = $row[MA_MEMBER_ATTRIBUTE_TABLE_FIELDNAME::NAME];
-    $avalue = $row[MA_MEMBER_ATTRIBUTE_TABLE_FIELDNAME::VALUE];
-    $aself = $row[MA_MEMBER_ATTRIBUTE_TABLE_FIELDNAME::SELF_ASSERTED];
-    // There must be a better way to convert to Boolean
-    $aself = ($aself !== "f");
-    $attr = array(MA_ATTRIBUTE::NAME => $aname,
-            MA_ATTRIBUTE::VALUE => $avalue,
-            MA_ATTRIBUTE::SELF_ASSERTED => $aself);
-    $attrs[] = $attr;
-  }
-  $result = array(MA_ARGUMENT::MEMBER_ID => $member_id,
-          MA_ARGUMENT::ATTRIBUTES => $attrs);
   return $result;
 }
 
