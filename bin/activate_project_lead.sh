fdab8526a12d3a8b155b5a67a37b331613460383
Tom Mitchell <tmitchell@bbn.com>
2012-09-13 14:38:11 -0400

    Update activate_project_lead to interact with ma.

--- bin/activate_project_lead.sh
+++ bin/activate_project_lead.sh
@@ -4,13 +4,28 @@
 # Usage: activate_project_lead.sh username
 
 if [ $# -ne 1 ]; then
-    echo "Usage: activate_project_lead.sh username"
+    echo "Usage: activate_project_lead.sh member_id"
     exit
 fi
 
-export p=`dirname $0`
+signer_cert=/usr/share/geni-ch/ma/ma-cert.pem
+signer_key=/usr/share/geni-ch/ma/ma-key.pem
+ma_host=`hostname --fqdn`
+url=https://$ma_host/ma/ma_controller.php
+plain_file=`mktemp`
+signed_file=`mktemp`
+result_file=`mktemp`
 
-$p/activate_account.sh $1
-$p/create_assertion.sh $1 lead resource
+msg="{\"operation\":\"add_member_privilege\",\"member_id\":\"$1\",\"privilege_id\":1}"
+echo $msg > $plain_file
 
-exit
+openssl smime -sign -signer $signer_cert -inkey $signer_key \
+  -in $plain_file -out $signed_file
+
+curl -i -X PUT --data-binary @$signed_file -o $result_file $url
+
+cat $result_file
+rm $plain_file
+rm $signed_file
+rm $result_file
+exit 0
