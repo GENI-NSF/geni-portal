27df0b39fa7d37b421d579c233622d58bb669c91
Sarah Edwards <sedwards@bbn.com>
2012-09-24 10:05:08 -0400

    Add port to ssh command pulled from manifest if it exists and if it's not 22.

--- portal/www/portal/print-text-helpers.php
+++ portal/www/portal/print-text-helpers.php
@@ -208,7 +208,11 @@ function print_rspec_pretty( $xml ){
     echo "<tr>\n";    
     if ($login){
       echo "<th colspan='2'>Login</th>\n";
-      echo "<td colspan='3'>ssh ", $login['username'],"@",$login['hostname'],"</td>\n";
+      echo "<td colspan='3'>ssh ", $login['username'],"@",$login['hostname'];
+      if ($login['port'] and !$login['port']==22 and !$login['port']=="22"){
+	echo " -p ", $login['port'];
+      }
+      echo "</td>\n";
     }
     echo "</tr>\n";
 
