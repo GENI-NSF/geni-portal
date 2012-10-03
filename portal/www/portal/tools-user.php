634759d9f08f68dbb88801e398a8e78b00bfd4ea
Aaron Helsinger <ahelsing@bbn.com>
2012-10-03 16:10:11 -0400

    Sort into Me and My Stuff. Tweak some SSH key wording. Note that modify is for being a project lead.

--- portal/www/portal/tools-user.php
+++ portal/www/portal/tools-user.php
@@ -28,6 +28,7 @@ require_once("cert_utils.php");
 require_once("rq_client.php");
 ?>
 <h1>User Tools</h1>
+<h1>About Me</h1>
 <?php
 /*----------------------------------------------------------------------
  * SSH key management
@@ -39,7 +40,7 @@ if (count($keys) == 0)
   {
     // No ssh keys are present.
     print "No SSH keys have been uploaded. ";
-    print "SSH keys are required to log in to many reserved compute resources.<br/><br/>\n";
+    print "SSH keys are required to log in to reserved compute resources.<br/><br/>\n";
     print "You can <button onClick=\"window.location='generatesshkey.php'\">generate and download an SSH keypair</button>";
     print "or <button onClick=\"window.location='uploadsshkey.php'\">upload an SSH public key</button>, if you have one you want to use.<br/>\n";
     print "If you're not sure what to do, choose 'Generate'.<br/>\n";
@@ -73,68 +74,16 @@ else
       . "</tr>\n";
     }
     print "</table>\n";
-  print "<i>Note</i>: You will need your SSH private key on your local machine. <br/>\nIf you generated your SSH keypair on this portal and have not already done so, be sure to Download your SSH key. <br/>\nAfter you download your key, be sure to set local permissions on that file appropriately. On Linux and Mac, do \"chmod 0600 <i>[path-to-SSH-private-key]</i>\". <br/>\nWhen you invoke SSH to log in to reserved resources, you will need to remember the path to that file. <br/>Your SSH command will be something like: \"ssh -i <i>path-to-SSH-key-you-downloaded</i> <i>[username]@[hostname]</i>\".<br/>\n";
+    print "<i>Note</i>: You will need your SSH private key on your local machine. <br/>\nIf you generated your SSH keypair on this portal and have not already done so, be sure to Download your SSH key. <br/>\nAfter you download your key, be sure to set local permissions on that file appropriately. On Linux and Mac, do \"chmod 0600 <i>[path-to-SSH-private-key]</i>\". <br/>\nWhen you invoke SSH to log in to reserved resources, you will need to remember the path to that file. <br/>Your SSH command will be something like: \"ssh -i <i>path-to-SSH-key-you-downloaded</i> <i>[username]@[hostname]</i>\".<br/>\n";
     print "<br/>\n";
     print "<button onClick=\"window.location='uploadsshkey.php'\">Upload another SSH public key</button>\n";
-  }
-
-
-/*----------------------------------------------------------------------
- * RSpecs
- *----------------------------------------------------------------------
- */
-print "<h2>Manage RSpecs</h2>\n";
-
-print "<button onClick=\"window.location='rspecs.php'\">"
-  . "Manage RSpecs</button>\n";
-
-/*----------------------------------------------------------------------
- * SSL key management
- *----------------------------------------------------------------------
- */
-print "<h2>Keys and Certificates for command line tools</h2>\n";
-print ("For <i>Advanced</i> users: download an SSL certificate and private key,"
-       . " in order to use other GENI tools, such as Omni.<br/><br/>\n");
-$keyrow = db_fetch_outside_private_key_cert($user->account_id);
-if ($keyrow) {
-  require_once("am_client.php");
-  // must double backslash things in the omni_config here....
-  $omni_config = get_template_omni_config($user);
-  $omni_config = str_replace("\n", "\\n", $omni_config);
-  $configalert = "Here is a template Omni config file.\\nTo use this:\\n\\t1. Save it to a file named portal_omni_config.\\n\\t2. Download your certificate, noting the path.\\n\\t3. Edit the portal_omni_config to correct \\n\\t\\t(a) the certificate path, \\n\\t\\t(b) the path to the SSL private key used to generate your certificate, and \\n\\t\\t(c) the path to your SSH public key to use for node logon.\\n\\t4. When running omni: \\n\\t\\ta) Do: omni -c portal_omni_config --slicecred <path to downloaded slice credential> ... to specify the path to this omni config and your downloaded slice credential\\n\\t\\tb) Use the full slice URN when naming your slice, not just the slice name\\n\\n$omni_config\\n";
-
-  print "\n<table>\n";
-  print "<tr><th>Certificate</th><th>Owner URN</th><th>Omni Config</th></tr>\n";
-  $download_url = relative_url("certificate.php");
-  $urn = urn_from_cert($keyrow['certificate']);
-
-  print "<tr>"
-    . "<td><button onClick=\"window.location='" . $download_url . "'\">Download Certificate</button></td>"
-    . "<td>$urn</td>"
-    . "<td><button onClick=\"JavaScript:alert('$configalert')\">Get Omni Config</button></td>"
-    . "</tr>\n";
-  print "</table>\n";
-  // FIXME: Way to delete a key?
-} else {
-  print "<button onClick=\"window.location='downloadkeycert.php'\">"
-    . "Generate certificate and key</button>\n";
-}
-
-
-/*----------------------------------------------------------------------
- * ABAC (if enabled)
- *----------------------------------------------------------------------
- */
-if ($portal_enable_abac)
-  {
-    print '<h2>ABAC</h2>\n';
-    print "<button onClick=\"window.location='abac-id.php'\">Download your ABAC ID</button><br/>\n";
-    print "<button onClick=\"window.location='abac-key.php'\">Download your ABAC private key</button>\n";
   }
 ?>
 
 <h2>Edit Account Details</h2>
-Modify user supplied account details <button onClick="window.location='modify.php'">here</button><br/>
+Modify user supplied account details <button
+onClick="window.location='modify.php'">here</button> (e.g. to become
+a Project Lead).<br/>
 
 <h2>Outstanding Requests</h2>
 <?php
@@ -201,3 +150,60 @@ print "<tr><th>GENI Username</th><td>" . $user->username . "</td></tr>\n";
 // FIXME: Other attributes?
 // FIXME: Permissions
 print "</table>\n";
+
+print "<h1>My Stuff</h1>\n";
+
+/*----------------------------------------------------------------------
+ * RSpecs
+ *----------------------------------------------------------------------
+ */
+print "<h2>Manage RSpecs</h2>\n";
+
+print "<button onClick=\"window.location='rspecs.php'\">"
+  . "Manage RSpecs</button>\n";
+
+/*----------------------------------------------------------------------
+ * SSL key management
+ *----------------------------------------------------------------------
+ */
+print "<h2>Keys and Certificates for command line tools</h2>\n";
+print ("For <i>Advanced</i> users: download an SSL certificate and private key,"
+       . " in order to use other GENI tools, such as Omni.<br/><br/>\n");
+$keyrow = db_fetch_outside_private_key_cert($user->account_id);
+if ($keyrow) {
+  require_once("am_client.php");
+  // must double backslash things in the omni_config here....
+  $omni_config = get_template_omni_config($user);
+  $omni_config = str_replace("\n", "\\n", $omni_config);
+  $configalert = "Here is a template Omni config file.\\nTo use this:\\n\\t1. Save it to a file named portal_omni_config.\\n\\t2. Download your certificate, noting the path.\\n\\t3. Edit the portal_omni_config to correct \\n\\t\\t(a) the certificate path, \\n\\t\\t(b) the path to the SSL private key used to generate your certificate, and \\n\\t\\t(c) the path to your SSH public key to use for node logon.\\n\\t4. When running omni: \\n\\t\\ta) Do: omni -c portal_omni_config --slicecred <path to downloaded slice credential> ... to specify the path to this omni config and your downloaded slice credential\\n\\t\\tb) Use the full slice URN when naming your slice, not just the slice name\\n\\n$omni_config\\n";
+
+  print "\n<table>\n";
+  print "<tr><th>Certificate</th><th>Owner URN</th><th>Omni Config</th></tr>\n";
+  $download_url = relative_url("certificate.php");
+  $urn = urn_from_cert($keyrow['certificate']);
+
+  print "<tr>"
+    . "<td><button onClick=\"window.location='" . $download_url . "'\">Download Certificate</button></td>"
+    . "<td>$urn</td>"
+    . "<td><button onClick=\"JavaScript:alert('$configalert')\">Get Omni Config</button></td>"
+    . "</tr>\n";
+  print "</table>\n";
+  // FIXME: Way to delete a key?
+} else {
+  print "<button onClick=\"window.location='downloadkeycert.php'\">"
+    . "Generate certificate and key</button>\n";
+}
+
+
+/*----------------------------------------------------------------------
+ * ABAC (if enabled)
+ *----------------------------------------------------------------------
+ */
+if ($portal_enable_abac)
+  {
+    print '<h2>ABAC</h2>\n';
+    print "<button onClick=\"window.location='abac-id.php'\">Download your ABAC ID</button><br/>\n";
+    print "<button onClick=\"window.location='abac-key.php'\">Download your ABAC private key</button>\n";
+  }
+?>
+
