ce24742ede988f5a26c9bf0cb048edb3532e7510
Tom Mitchell <tmitchell@bbn.com>
2012-10-05 10:10:21 -0400

    Add portal to include path.
    Hardcoded for now. Can be extracted in the future.

--- openid/src/server-indirect/server.php
+++ openid/src/server-indirect/server.php
@@ -3,6 +3,12 @@
 $path_extra = dirname(dirname(dirname(__FILE__)));
 $path = ini_get('include_path');
 $path = $path_extra . PATH_SEPARATOR . $path;
+// Ugh. Hardcode the path to the portal code. Because the OpenID
+// indirect server (the one the browser gets redirected to) sits
+// inside https://xyz.com/secure, it gets the include path inherited
+// from the /secure definition in apache2.conf. But it needs access to
+// portal code. So for now, hardcode the path.
+$path = '/var/www/secure' . PATH_SEPARATOR . $path;
 ini_set('include_path', $path);
 
 $try_include = @include 'config.php';
