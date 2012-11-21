02fc0c9a043b394754bf4a22e39e1edfa826f3d8
Sarah Edwards <sedwards@bbn.com>
2012-11-21 16:42:09 -0600

    Put js in an external file.

---
+++ portal/www/portal/ajax.js
@@ -0,0 +1,61 @@
+function build_agg_table() 
+{
+   // (1) query the server for all a list of aggregates
+   $.getJSON("aggregates.php",function(responseTxt,statusTxt,xhr){
+   if(statusTxt=="success") 
+   {
+     var json_agg;
+     var name;
+     var output; 
+     // (2) create an HTML table with one row for each aggregate
+     json_agg = responseTxt;
+     output = "<table id='status_table'>";
+     for (am_id in json_agg ) {
+	    agg = json_agg[am_id];                    
+	    name = agg.name;
+            output += "<tr id='"+am_id+"'><td>";
+	    output += "<button id='hello' type='button' onclick='update_agg_row("+am_id+")'>Reload</button>";
+	    output += "</td><td id='status_"+am_id+"' class='updating'>";	
+	    output += "...updating...";
+	    output += "</td><td>";	
+	    output += name;
+//	    output += "</td><td>";	
+//	    output += agg.url;
+	    output += "</td></tr>";
+            // (3) Get the status for this slice at this aggregate
+	    update_agg_row( am_id );
+     }	
+     output += "</table>";
+     $("#status_table_div").html(output);
+   }
+   if(statusTxt=="error")
+     alert("Error: "+xhr.status+": "+xhr.statusText);
+   });
+}
+
+
+function update_agg_row(am_id) {
+  // This queries for the json file at (for example):
+  // https://sergyar.gpolab.bbn.com/secure/amstatus.php?am_id=9&slice_id=b18cb314-c4dd-4f28-a6fd-b355190e1b61
+  $.getJSON("amstatus.php", { am_id:am_id, slice_id:slice },function(responseTxt,statusTxt,xhr){
+     if(statusTxt=="success") 
+     {
+        var json_am;
+        var am;
+        var geni_status;
+        var output=""; 
+        json_am = responseTxt;
+        for (new_id in json_am ) {	
+           am = json_am[new_id];	   
+           geni_status = am['geni_status'];
+    	   output += geni_status;
+        }
+        $("td#status_"+am_id).text( output );
+//        $("td#status_"+am_id).style.color="blue";
+     }
+     if(statusTxt=="error")
+        alert("Error: "+xhr.status+": "+xhr.statusText);
+  });  
+}
+
+$(document).ready(build_agg_table);
