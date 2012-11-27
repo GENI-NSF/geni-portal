2e6bd45667043f0e1cc3fada946f9902273bd059
Sarah Edwards <sedwards@bbn.com>
2012-11-27 12:05:40 -0600

    Hide sliver action buttons when AM status is undefined.

--- portal/www/portal/ajax.js
+++ portal/www/portal/ajax.js
@@ -36,7 +36,7 @@ function build_agg_table_on_slicepg()
 	    name = agg.name;
             output += "<tr id='"+am_id+"'>";
 	    output += "<td class='notapply'></td>";
-	    output += "<td><button id='hello' type='button' onclick='refresh_agg_row("+am_id+")'>Reload</button>";
+	    output += "<td><button id='reload_button_'"+am_id+" type='button' onclick='refresh_agg_row("+am_id+")'>Reload</button>";
 	    output += "</td><td id='status_"+am_id+"' class='updating'>";	
 	    output += updating_text;
 	    output += "</td><td>";	
@@ -55,9 +55,9 @@ function build_agg_table_on_slicepg()
 	    }
 	    // sliver actions
 	    output += "<td>";
-	    output += "<button onClick=\"window.location='"+status_url+"&am_id="+am_id+"'\"><b>Resource Status</b></button>";
-	    output += "<button title='Login info, etc' onClick=\"window.location='"+listres_url+"&am_id="+am_id+"'\"><b>Details</b></button>\n";
-	    output += "<button onClick=\"window.location='confirm-sliverdelete.php?slice_id=" + slice+ "&am_id="+am_id+"'\" "+ delete_slivers_disabled +"><b>Delete Resources</b></button>\n";
+	    output += "<button id='status_button_"+am_id+"' onClick=\"window.location='"+status_url+"&am_id="+am_id+"'\"><b>Resource Status</b></button>";
+	    output += "<button  id='details_button_"+am_id+"' title='Login info, etc' onClick=\"window.location='"+listres_url+"&am_id="+am_id+"'\"><b>Details</b></button>\n";
+	    output += "<button  id='delete_button_"+am_id+"' onClick=\"window.location='confirm-sliverdelete.php?slice_id=" + slice+ "&am_id="+am_id+"'\" "+ delete_slivers_disabled +"><b>Delete Resources</b></button>\n";
 	    output += "</td></tr>";
             // (3) Get the status for this slice at this aggregate
 	    update_agg_row( am_id );
@@ -88,7 +88,7 @@ function build_agg_table()
 	    name = agg.name;
             output += "<tr id='"+am_id+"'>";
 	    output += "<td class='notapply'></td>";
-	    output += "<td><button id='hello' type='button' onclick='update_agg_row("+am_id+")'>Reload</button>";
+	    output += "<td><button id='button_'"+am_id+" type='button' onclick='update_agg_row("+am_id+")'>Reload</button>";
 	    output += "</td><td id='status_"+am_id+"' class='updating'>";	
 	    output += "...updating...";
 	    output += "</td><td>";	
@@ -109,7 +109,7 @@ function build_agg_table()
 
 
 function refresh_agg_row(am_id) {
-
+ 
     geni_status = "updating"
     $("td#status_"+am_id).text( updating_text);
     $("td#status_"+am_id).attr( "class", geni_status );
@@ -119,6 +119,7 @@ function refresh_agg_row(am_id) {
 function update_agg_row(am_id) {
   // This queries for the json file at (for example):
   // https://sergyar.gpolab.bbn.com/secure/amstatus.php?am_id=9&slice_id=b18cb314-c4dd-4f28-a6fd-b355190e1b61
+  $("button#reload_button_"+am_id).prop( "disabled", true ); 
   $.getJSON("amstatus.php", { am_id:am_id, slice_id:slice },function(responseTxt,statusTxt,xhr){
      if(statusTxt=="success") 
      {
@@ -134,6 +135,16 @@ function update_agg_row(am_id) {
         }
         $("td#status_"+am_id).text( output );
         $("td#status_"+am_id).attr( "class", geni_status );
+
+	if (geni_status == undefined){
+	    $("button#status_button_"+am_id).prop( "disabled", true ); 
+	    $("button#details_button_"+am_id).prop( "disabled", true ); 
+	    $("button#delete_button_"+am_id).prop( "disabled", true ); 
+	} else {
+	    $("button#status_button_"+am_id).removeProp( "disabled"); 
+	    $("button#details_button_"+am_id).removeProp( "disabled"); 
+	    $("button#delete_button_"+am_id).removeProp( "disabled"); 
+	}
      }
      if(statusTxt=="error")
         alert("Error: "+xhr.status+": "+xhr.statusText);
