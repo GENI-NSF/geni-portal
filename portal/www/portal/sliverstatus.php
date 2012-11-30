fa75d5e557168693284ffdf6a761e5b13ab5aafa
Sarah Edwards <sedwards@bbn.com>
2012-11-30 16:06:31 -0600

    Move some variables into a file, status_constants_import.php.

--- portal/www/portal/sliverstatus.php
+++ portal/www/portal/sliverstatus.php
@@ -75,42 +75,14 @@ $header = "Status of Slivers on slice: $slice_name";
 
 show_header('GENI Portal: Slices',  $TAB_SLICES);
 include("tool-breadcrumbs.php");
-
 ?>
 
 <script src="amstatus.js"></script>
+
 <script>
 var slice= "<?php echo $slice_id ?>";
-// var am_id= "<?php echo $am_id ?>";
 
-var GENI_READY = "<?php echo STATUS_INDEX::GENI_READY ?>";
-var GENI_NO_RESOURCES = "<?php echo STATUS_INDEX::GENI_NO_RESOURCES ?>";
-var GENI_BOOTING = "<?php echo STATUS_INDEX::GENI_BOOTING ?>";
-var GENI_BUSY = "<?php echo STATUS_INDEX::GENI_BUSY ?>";
-
-var GENI_READY_STR = "<?php echo STATUS_MSG::GENI_READY ?>";
-var GENI_NO_RESOURCES_STR = "<?php echo STATUS_MSG::GENI_NO_RESOURCES ?>";
-var GENI_BOOTING_STR = "<?php echo STATUS_MSG::GENI_BOOTING ?>";
-var GENI_BUSY_STR = "<?php echo STATUS_MSG::GENI_BUSY ?>";
-
-var GENI_READY_CLASS = "<?php echo STATUS_CLASS::GENI_READY ?>";
-var GENI_NO_RESOURCES_CLASS = "<?php echo STATUS_CLASS::GENI_NO_RESOURCES ?>";
-var GENI_BOOTING_CLASS = "<?php echo STATUS_CLASS::GENI_BOOTING ?>";
-var GENI_BUSY_CLASS = "<?php echo STATUS_CLASS::GENI_BUSY ?>";
-
-var GENI_MESSAGES;
-GENI_MESSAGES = Array();
-GENI_MESSAGES[ GENI_READY ] = GENI_READY_STR;
-GENI_MESSAGES[ GENI_NO_RESOURCES ] = GENI_NO_RESOURCES_STR;
-GENI_MESSAGES[ GENI_BOOTING ] = GENI_BOOTING_STR;
-GENI_MESSAGES[ GENI_BUSY ] = GENI_BUSY_STR;
-
-var GENI_CLASSES;
-GENI_CLASSES = Array();
-GENI_CLASSES[ GENI_READY ] = GENI_READY_CLASS;
-GENI_CLASSES[ GENI_NO_RESOURCES ] = GENI_NO_RESOURCES_CLASS;
-GENI_CLASSES[ GENI_BOOTING ] = GENI_BOOTING_CLASS;
-GENI_CLASSES[ GENI_BUSY ] = GENI_BUSY_CLASS;
+<?php include('status_constants_import.php'); ?>
 
 $(document).ready(build_agg_table_on_sliverstatuspg);
 </script>
