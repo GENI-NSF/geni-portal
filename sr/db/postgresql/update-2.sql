2aa0980896ff4b21875fd82e6778a2789138674d
Aaron Helsinger <ahelsing@bbn.com>
2013-03-29 13:21:10 -0400

    add the exo AMs as +am not +cm

--- sr/db/postgresql/update-2.sql
+++ sr/db/postgresql/update-2.sql
@@ -2,16 +2,16 @@
 
 -- eg gpo
 UPDATE service_registry
-  SET service_urn = 'urn:publicid:IDN+exogeni.net:bbnvmsite+authority+cm'
+  SET service_urn = 'urn:publicid:IDN+exogeni.net:bbnvmsite+authority+am'
   WHERE service_url = 'https://bbn-hn.exogeni.net:11443/orca/xmlrpc';
 
 -- eg renci
 UPDATE service_registry
-  SET service_urn = 'urn:publicid:IDN+exogeni.net:rcivmsite+authority+cm'
+  SET service_urn = 'urn:publicid:IDN+exogeni.net:rcivmsite+authority+am'
   WHERE service_url = 'https://rci-hn.exogeni.net:11443/orca/xmlrpc';
 
 -- eg exosm
 UPDATE service_registry
-  SET service_urn = 'urn:publicid:IDN+exogeni.net+authority+cm'
+  SET service_urn = 'urn:publicid:IDN+exogeni.net+authority+am'
   WHERE service_url = 'https://geni.renci.org:11443/orca/xmlrpc';
 
