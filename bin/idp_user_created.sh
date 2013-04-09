c455f3bc167305026f2ec3ff06129a7aea9faf41
Aaron Helsinger <ahelsing@bbn.com>
2013-04-09 17:05:23 -0400

    mark a user in the idp_account_request table as having been created - taken by email address

---
+++ bin/idp_user_created.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: idp_user_created <email>"
+    exit
+fi
+
+if [ "$1" = "" ]; then
+    echo "Usage: idp_user_created <email>"
+    exit
+fi
+
+if [ -f $HOME/.pgpass ]; then
+    export PGPASSFILE="$HOME/.pgpass"
+else
+    export PGPASSFILE="/usr/local/etc/psql_password"
+fi
+
+echo "update idp_account_request set created_ts=now() where created_ts is null and email='$1'" | psql -U portal -h localhost portal
