bef5a8ec80bb72b476cc90a68ad49199c1310b49
Marshall Brinn <mbrinn@bbn.com>
2013-05-01 09:40:42 -0400

    Remove a member from slices when removing from project, amking project lead to slice lead if necessary

--- portal/www/portal/do-edit-project-member.php
+++ portal/www/portal/do-edit-project-member.php
@@ -26,7 +26,9 @@ require_once("user.php");
 require_once("header.php");
 require_once('util.php');
 require_once('pa_constants.php');
+require_once('sa_constants.php');
 require_once('pa_client.php');
+require_once('sa_client.php');
 require_once('sr_constants.php');
 require_once('sr_client.php');
 
@@ -48,6 +50,84 @@ if (!isset($user) || is_null($user) || ! $user->isActive()) {
 
 if (! isset($pa_url)) {
   $pa_url = get_first_service_of_type(SR_SERVICE_TYPE::PROJECT_AUTHORITY);
+}
+
+if (! isset($sa_url)) {
+  $sa_url = get_first_service_of_type(SR_SERVICE_TYPE::SLICE_AUTHORITY);
+}
+
+// Add project lead as lead of slice
+// Change to lead if already a member
+function add_project_lead_as_slice_lead($slice_id, $project_members_by_role, $slice_members)
+{
+  global $sa_url;
+  global $user;
+
+  $project_lead = null;
+  //  error_log("PMBRS = " . print_r($project_members_by_role, true));
+  foreach($project_members_by_role as $pm => $pm_role) {
+    if($pm_role == CS_ATTRIBUTE_TYPE::LEAD) {
+      $project_lead = $pm;
+      break;
+    }
+  }
+
+  $project_lead_is_slice_member = false;
+  foreach($slice_members as $slice_member) {
+    $slice_member_id = $slice_member[SA_SLICE_MEMBER_TABLE_FIELDNAME::MEMBER_ID];
+    if ($slice_member_id == $project_lead) {
+      $project_lead_is_slice_member = true;
+      break;
+    }
+  }
+  //  error_log("Lead is in project: " .  $project_lead_is_slice_member);
+
+  // If project lead is already a member, make into lead
+  // Otherwise add as lead
+  if($project_lead_is_slice_member) {
+    error_log("Changing " . $project_lead . " to lead of slice " . $slice_id);
+    change_slice_member_role($sa_url, $user, $slice_id, $project_lead, CS_ATTRIBUTE_TYPE::LEAD);
+  } else {
+    error_log("Adding " . $project_lead . " to lead of slice " . $slice_id);
+    add_slice_member($sa_url, $user, $slice_id, $project_lead, CS_ATTRIBUTE_TYPE::LEAD);
+  }
+
+}
+
+// Remove a member from a project
+// Find all slices to which the member belongs of this project and remove from them as well
+// If the member is the lead of the slice, make the project lead the member of the slice
+// When all this is done, remove member from project
+function remove_project_member_from_project_and_slices($project_id, $project_member_id, $project_members_by_role)
+{
+  global $sa_url;
+  global $pa_url;
+  global $user;
+
+  // Get the project slices and memberships
+  $slice_members = get_slice_members_for_project($sa_url, $user, $project_id);
+  //  error_log("SM = " . print_r($slice_members, true));
+  foreach($slice_members as $slice_member) {
+    $removed_lead = false;
+    $slice_id = $slice_member[SA_SLICE_MEMBER_TABLE_FIELDNAME::SLICE_ID];
+    $slice_member_id = $slice_member[SA_SLICE_MEMBER_TABLE_FIELDNAME::MEMBER_ID];
+    $slice_member_role = $slice_member[SA_SLICE_MEMBER_TABLE_FIELDNAME::ROLE];
+    if ($slice_member_id == $project_member_id) {
+      error_log("   Removing " . $slice_member_id . " from slice " . $slice_id . " " . $slice_member_role);
+      if ($slice_member_role == CS_ATTRIBUTE_TYPE::LEAD) {
+	$removed_lead = true;
+      }
+    }
+
+    //      remove_slice_member($sa_url, $user, $slice_id, $slice_member_id);
+    if($removed_lead) {
+      error_log("Removed slice lead : " . $slice_id . " " . $project_member_id);
+      add_project_lead_as_slice_lead($slice_id, $project_members_by_role, $slice_members);
+    }
+  }
+
+  error_log("Removing " . $project_member_id . " from project " . $project_id);
+  remove_project_member($pa_url, $user, $project_id, $project_member_id);
 }
 
 // Ensure that the new roles maintains a single project lead
@@ -105,8 +185,8 @@ function modify_project_membership($project_id, $member_id, $selection_id,
   if($is_member) {
     $role = $project_members_by_role[$member_id];
     if ($selection_id == 0) {
-      // Remove this member from this project
-      remove_project_member($pa_url, $user, $project_id, $member_id);
+      // Remove this member from this project and associated slices
+      remove_project_member_from_project_and_slices($project_id, $member_id, $project_members_by_role);
     } else if ($selection_id != $role) {
       // Change the role of this member in this project
       change_member_role($pa_url, $user, $project_id, $member_id, $selection_id);
