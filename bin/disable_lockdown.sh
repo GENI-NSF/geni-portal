fcd90de16b60d43c5383a7789e70e95be0c042ef
Aaron Helsinger <ahelsing@bbn.com>
2013-05-28 22:03:15 -0400

    scripts to disable need sudo as well

--- bin/disable_lockdown.sh
+++ bin/disable_lockdown.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-# Script to enter a portal/clearinghouse installation into 'lockdown mode'
+# Script to exit a portal/clearinghouse installation out of 'lockdown mode'
 # Lockdown mode means that existing users can continue to use existing
 # slices and slivers but can't create new slices, renew existng slices
 # create new projects
 # download or upload crypto materials
 # or make changes to properties of slices and projects including membership
+
 psql -U portal -h localhost portal < /usr/share/geni-ch/cs/db/postgresql/disable_lockdown.sql
-~/proto-ch/bin/geni-manage-maintenance --clear-alert
-~/proto-ch/bin/geni-manage-maintenance --clear-lockdown
+
+sudo ~/proto-ch/bin/geni-manage-maintenance --clear-alert
+
+sudo ~/proto-ch/bin/geni-manage-maintenance --clear-lockdown
 
--- bin/disable_sundown.sh
+++ bin/disable_sundown.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Script to exit portal/clearinghouse into 'sundown mode'
+# Script to exit portal/clearinghouse out of 'sundown mode'
 # Sundown mode means that new or existing slices (and thus slivers)
 # cannot have expiration times that go beyond a specified date
-~/proto-ch/bin/geni-manage-maintenance --clear-sundown
+
+sudo ~/proto-ch/bin/geni-manage-maintenance --clear-sundown
 
