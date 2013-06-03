63fd60e5a94052303c19849775edc406cb5f9003
Aaron Helsinger <ahelsing@bbn.com>
2013-06-03 14:08:41 -0400

    complete the addition of the NICTA AM

--- bin/add_exo_nicta.sql
+++ bin/add_exo_nicta.sql
@@ -15,12 +15,30 @@ insert into service_registry
       0,
       -- URL
      'https://nicta-hn.exogeni.net:11443/orca/xmlrpc',
-      -- CERT (none yet for NICTA rack)
-     '',
+      -- CERT
+     '/usr/share/geni-ch/sr/certs/exo-nicta-am.pem',
       -- NAME
      'NICTA ExoGENI',
       -- DESCRIPTION
      'ExoGENI NICTA Rack',
       -- URN
      'urn:publicid:IDN+exogeni.net:nictavmsite+authority+am'
+    );
+
+insert into service_registry
+    (service_type, service_url, service_cert, service_name,
+     service_description, service_urn)
+  values
+    ( -- TYPE: 7 = CA
+      7,
+      -- URL
+     '',
+      -- CERT (self signed)
+     '/usr/share/geni-ch/sr/certs/exo-nicta-am.pem',
+      -- NAME
+     '',
+      -- DESCRIPTION
+     'ExoGENI NICTA Rack cert signer (self)',
+      -- URN
+     ''
     );
