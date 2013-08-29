0cdb0dad2d10cf31f00ead1407c221c3e6e73e7a
Aaron Helsinger <ahelsing@bbn.com>
2013-08-29 14:37:02 -0400

    Ticket 779: Add sql scripts for adding the GPO FOAM and IG GPO FOAMs to the SR

---
+++ bin/add-gpo-of.sql
@@ -0,0 +1,44 @@
+-- -----------------------------------------------------------------
+-- Create the entry for GPO FOAM aggregate:
+--
+-- Execute as:
+--
+--    psql -U portal -h localhost -f add-gpo-of.sql portal
+--
+-- -----------------------------------------------------------------
+
+insert into service_registry
+    (service_type, service_url, service_cert, service_name,
+     service_description, service_urn)
+  values
+    ( -- TYPE: zero = aggregate
+      0,
+      -- URL
+      'https://foam.gpolab.bbn.com:3626/foam/gapi/2',
+      -- CERT
+     '/usr/share/geni-ch/sr/certs/gpo-of.pem',
+      -- NAME
+     'InstaGENI OpenFlow',
+      -- DESCRIPTION
+     'GPO Rack OpenFlow',
+      -- URN
+     'urn:publicid:IDN+openflow:foam:foam.gpolab.bbn.com+authority+am'
+    );
+
+insert into service_registry
+    (service_type, service_url, service_cert, service_name,
+     service_description, service_urn)
+  values
+    ( -- TYPE: 7 = CA
+      7,
+      -- URL
+     '',
+      -- CERT (self signed)
+     '/usr/share/geni-ch/sr/certs/gpo-of.pem',
+      -- NAME
+     '',
+      -- DESCRIPTION
+     'GPO FOAM cert signer (self)',
+      -- URN
+     ''
+    );
