ca0eacf6986636b95f05f8bde1afa5a04df8a29d
Tom Mitchell <tmitchell@bbn.com>
2013-12-11 17:56:19 -0500

    Clarify warning message with one aggregate.
    When deleting resources at a single aggregate, make the confirmation
    question indicate that. When deleting from all aggregates, say so
    clearly in the message.

--- portal/www/portal/confirm-sliverdelete.php
+++ portal/www/portal/confirm-sliverdelete.php
@@ -47,6 +47,11 @@ if (isset($slice_expired) && convert_boolean($slice_expired)) {
   relative_redirect('slices.php');
 }
 
+if (! isset($slice_id)) {
+  /* How did they get here without a slice id? */
+  relative_redirect('home.php');
+}
+
 if (!$user->isAllowed(SA_ACTION::DELETE_SLIVERS, CS_CONTEXT_TYPE::SLICE, $slice_id)) {
   relative_redirect('home.php');
 }
@@ -55,21 +60,26 @@ if (isset($slice) && $slice != "None") {
   $slice_name = $slice[SA_ARGUMENT::SLICE_NAME];
 }
 
+/* Set up defaults, override if AM is specified. */
+$am_name = None;
+$confirm_msg = 'Delete all reserved resources at all aggregates?';
+$edit_url = "sliverdelete.php?slice_id=$slice_id";
+if (isset($am_id) && $am_id) {
+  $edit_url = "sliverdelete.php?slice_id=$slice_id&am_id=$am_id";
+  $am_name = $ams[0][SR_ARGUMENT::SERVICE_NAME];
+  $confirm_msg = "Delete all reserved resources at $am_name?";
+}
+
 show_header('GENI Portal: Slices', $TAB_SLICES);
 print "<h2>Delete resources from GENI Slice: " . "<i>" . $slice_name . "</i>" . "</h2>\n";
-
-print "<p class='warn'>Delete all reserved resources? Otherwise click <b>Cancel</b>.</p>\n";
-
-$cancel_url = "slice.php?slice_id=$slice_id";
-if (isset($am_id) && $am_id) {
-  $edit_url = "sliverdelete.php?slice_id=$slice_id"."&am_id=$am_id";
-} else {
-  $edit_url = "sliverdelete.php?slice_id=$slice_id";
-}
+print "<p class='warn'>$confirm_msg Otherwise click <b>Cancel</b>.</p>\n";
 print "<p><button onclick=\"window.location='$edit_url'\"><b>Delete Resources</b></button> \n";
-//print "<button onclick=\"window.location='$cancel_url'\">Cancel</button>\n";
-print "<button onclick=\"history.back(-1)\">Cancel</button></p>\n";
-
+if (array_key_exists('HTTP_REFERER', $_SERVER)) {
+  print "<button onclick=\"history.back(-1)\">Cancel</button></p>\n";
+} else {
+  $cancel_url = "slice.php?slice_id=$slice_id";
+  print "<button onclick=\"window.location='$cancel_url'\">Cancel</button>\n";
+}
 
 include("footer.php");
 ?>
