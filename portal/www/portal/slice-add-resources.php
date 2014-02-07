6fde8a91eae42cb9b83700dc5064e680dfdfbc1d
Marshall Brinn <mbrinn@bbn.com>
2014-02-07 10:57:54 -0500

    Fix some wording per Niky's feedback

--- portal/www/portal/slice-add-resources.php
+++ portal/www/portal/slice-add-resources.php
@@ -76,7 +76,7 @@ function show_rspec_chooser($user) {
   //  print "<option value=\"upload\" title=\"Upload an RSpec\">Upload</option>\n";
   print "</select>\n";
 
-  print "<br>or <a href=\"rspecupload.php\">upload your own RSpec</a>.";
+  print "<br>or <a href=\"rspecupload.php\">upload your own RSpec to the above list</a>.";
 //  print " or <button onClick=\"window.location='rspecupload.php'\">";
 //  print "upload your own RSpec</button>.";
   // RSpec entry area
@@ -188,6 +188,7 @@ print '<form id="f1" action="sliceresource.php" method="post" enctype="multipart
 show_rspec_chooser($user);
 print  '<p><label for="file">or select RSpec file:</label>';
 print  '<input type="file" name="rspec_selection" id="rspec_selection" /></p>';
+print "<p><i>Note: This RSpec selection is for this reservation only and will not be saved by the portal.</i></p>";
 show_am_chooser();
 print '<input type="hidden" name="slice_id" value="' . $slice_id . '"/>';
 print '</form>';
