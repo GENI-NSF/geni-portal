c88d5fd24aaa587c01f2a8e0442b4b82d5c9cfbb
Ben Ujcich <bujcich@bbn.com>
2014-06-19 14:24:39 -0400

    Added stitcher copy of sliceresource

---
+++ portal/www/portal/stitcher_sliceresource.php
@@ -0,0 +1,154 @@
+<?php
+//----------------------------------------------------------------------
+// Copyright (c) 2011-2014 Raytheon BBN Technologies
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and/or hardware specification (the "Work") to
+// deal in the Work without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Work, and to permit persons to whom the Work
+// is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Work.
+//
+// THE WORK IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE WORK OR THE USE OR OTHER DEALINGS
+// IN THE WORK.
+//----------------------------------------------------------------------
+?>
+<?php
+require_once("settings.php");
+require_once('portal.php');
+require_once("user.php");
+require_once("file_utils.php");
+require_once("sr_client.php");
+require_once("sr_constants.php");
+require_once("am_client.php");
+require_once("am_map.php");
+require_once("sa_client.php");
+require_once("print-text-helpers.php");
+require_once("logging_client.php");
+$user = geni_loadUser();
+if (!isset($user) || is_null($user) || ! $user->isActive()) {
+  relative_redirect('home.php');
+}
+
+
+function no_slice_error() {
+  header('HTTP/1.1 404 Not Found');
+  print 'No slice id specified.';
+  exit();
+}
+function no_rspec_error() {
+  header('HTTP/1.1 404 Not Found');
+  if (array_key_exists("rspec_id", $_REQUEST)) {
+    $rspec_id = $_REQUEST['rspec_id'];
+    print "Invalid resource specification id \"$rspec_id\" specified.";
+  } else {
+    print 'No resource specification id specified.';
+  }
+  exit();
+}
+function no_am_error() {
+  header('HTTP/1.1 404 Not Found');
+  if (array_key_exists("am_id", $_REQUEST)) {
+    $am_id = $_REQUEST['am_id'];
+    print "Invalid aggregate manager id \"$am_id\" specified.";
+  } else {
+    print 'No aggregate manager id specified.';
+  }
+  exit();
+}
+
+if (! count($_REQUEST)) {
+  // No parameters. Return an error result?
+  // For now, return nothing.
+  no_slice_error();
+}
+unset($slice);
+unset($rspec);
+unset($am);
+include("tool-lookupids.php");
+if (! isset($slice)) {
+  no_slice_error();
+}
+
+if(array_key_exists('rspec_selection', $_FILES)) {
+  $local_rspec_file = $_FILES['rspec_selection']['tmp_name'];
+  $local_rspec_file = trim($local_rspec_file);
+  $temp_rspec_file = null;
+  if(strlen($local_rspec_file) > 0) {
+    $rspec = file_get_contents($local_rspec_file);
+    $temp_rspec_file = writeDataToTempFile($rspec, 'rspec-');
+  }
+}
+else if(array_key_exists('rspec_jacks', $_REQUEST)) {
+	$temp_rspec_file = null;
+  $local_rspec_file = $_REQUEST['rspec_jacks'];
+  if(strlen($local_rspec_file) > 0) {
+    $rspec = $local_rspec_file;
+    $temp_rspec_file = writeDataToTempFile($rspec, 'rspec-');
+  }
+}
+
+if (isset($slice_expired) && convert_boolean($slice_expired)) {
+  if (! isset($slice_name)) {
+    $slice_name = "";
+  }
+  $_SESSION['lasterror'] = "Slice " . $slice_name . " is expired.";
+  relative_redirect('slices.php');
+}
+
+if(!$user->isAllowed(SA_ACTION::LOOKUP_SLICE, CS_CONTEXT_TYPE::SLICE, $slice_id)) {
+  relative_redirect('home.php');
+}
+
+if (! isset($am) || is_null($am)) {
+  no_am_error();
+}
+
+// Get an AM
+$am_url = $am[SR_ARGUMENT::SERVICE_URL];
+$AM_name = am_name($am_url);
+
+$header = "Creating Sliver on slice: $slice_name";
+
+show_header('GENI Portal: Slices',  $TAB_SLICES);
+include("tool-breadcrumbs.php");
+?>
+
+<script>
+var slice= "<?php echo $slice_id ?>";
+var am_id= "<?php echo $am_id ?>";
+var rspec_id= "<?php echo $rspec_id ?>";
+var rspec_file = "<?php echo $temp_rspec_file ?>";
+function build_pretty_xml() 
+{
+  $("#prettyxml").load("createsliver.php", { slice_id:slice, rspec_id:rspec_id, am_id:am_id, rspec_file:rspec_file } );
+}
+</script>
+<script>
+$(document).ready(build_pretty_xml);
+</script>
+
+<?php
+print "<h2>$header</h2>\n";
+
+//print "Reserved resources on AM (<b>$AM_name</b>) until <b>$slice_expiration</b>:";
+print "<p>Resources on AM (<b>$AM_name</b>):</p>";
+print "<div class='resources' id='prettyxml'>";
+print "<p><i>Adding resources...</i></p>";
+print "</div>\n";
+
+print "<hr/>";
+print "<p><a href='slices.php'>Back to All slices</a>";
+print "<br/>";
+print "<a href='slice.php?slice_id=$slice_id'>Back to Slice <i>$slice_name</i></a></p>";
+include("footer.php");
+?>
