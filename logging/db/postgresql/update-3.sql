f0932a2e7345a47452c12b0c6c279e1ce0147485
Aaron Helsinger <ahelsing@bbn.com>
2014-07-29 13:20:49 -0400

    cannot alter the _old tables cause there's no update script that created them. So create them now.

--- logging/db/postgresql/update-3.sql
+++ logging/db/postgresql/update-3.sql
@@ -11,10 +11,19 @@ ALTER TABLE logging_entry_attribute
    ADD FOREIGN KEY (event_id) REFERENCES logging_entry(id),
    ALTER COLUMN attribute_name SET NOT NULL;
 
-ALTER TABLE logging_entry_old
-   ALTER COLUMN event_time SET NOT NULL;
+-- Somehow these 2 tables were never created by a previous update script, so do it now
 
-ALTER TABLE logging_entry_attribute_old
-   ALTER COLUMN event_id SET NOT NULL,
-   ADD FOREIGN KEY (event_id) REFERENCES logging_entry_old(id),
-   ALTER COLUMN attribute_name SET NOT NULL;
+CREATE TABLE logging_entry_old (
+  id SERIAL,
+  event_time TIMESTAMP NOT NULL,
+  user_id UUID,
+  message VARCHAR,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE logging_entry_attribute_old (
+  event_id INT NOT NULL REFERENCES logging_entry_old(id),
+  attribute_name VARCHAR NOT NULL,
+  attribute_value VARCHAR
+);
+
