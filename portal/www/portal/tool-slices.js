9c61f9414a4a6e5a34fc3793ac4d9f2386cd8e01
Tom Mitchell <tmitchell@bbn.com>
2014-09-17 15:43:43 -0400

    Remove debug line

--- portal/www/portal/tool-slices.js
+++ portal/www/portal/tool-slices.js
@@ -4,7 +4,6 @@ function info_set_location(slice_id, url)
               function (responseTxt, statusTxt, xhr) {
                   var json_agg = responseTxt;
                   var agg_ids = Object.keys(json_agg);
-                  agg_ids = [80, 82, 109];
                   var agg_count = agg_ids.length;
                   if (agg_count > 0) {
                       for (var i = 0; i < agg_count; i++) {
