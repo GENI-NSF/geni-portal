dff1c2c921aeddda9d7f04ae3f157be5d98862ca
Aaron Helsinger <ahelsing@bbn.com>
2014-12-17 11:06:38 -0500

    bump to 2.25; note that as committed, this uses emulab.net for jacks/flack while master uses portal.geni.net

--- lib/php/settings.php
+++ lib/php/settings.php
@@ -50,7 +50,7 @@ $portal_private_key_file = '/usr/share/geni-ch/portal/portal-key.pem';
 $portal_max_slice_renewal_days = 185;
 
 // Portal version
-$portal_version = "2.24";
+$portal_version = "2.25";
 
 // URL to the Flack loader. Used in flack.php
 $flack_url = "https://www.emulab.net/protogeni/flack-stable/loader.js";
