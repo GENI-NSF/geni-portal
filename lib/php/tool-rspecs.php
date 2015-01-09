8e61ddb05dad341cf9f19303545671d4cfc8926c
Marshall Brinn <mbrinn@bbn.com>
2015-01-09 09:54:08 -0500

    Merge ttk1143 with develop

--- lib/php/tool-rspecs.php
+++ lib/php/tool-rspecs.php
@@ -91,7 +91,9 @@ $owners = ma_lookup($ma_url, $user, $public_owners);
 
 /* Display starts here. */
 include('tool-rspecs.js');
+error_log("JSU = " . $jacks_stable_url);
 print '<script src="' . $jacks_stable_url . '"></script>';
+print "<div id='jacksEditorContainer' class='jacks' style='background-color: white; display:none;'></div>";
 print "<div id='jacksContainer' class='jacks' style='background-color: white; display:none;'></div>";
 print("<h2>Manage Resource Specifications (RSpecs)</h2>\n");
 print("<p>From this page you can ");
