35e2d9858f6762235a85cf452c6c2d5d807696a5
Marshall Brinn <mbrinn@bbn.com>
2015-01-22 12:57:29 -0500

    Remove status buttons: now details buttons show details AND status

--- portal/www/portal/slice-jacks.php
+++ portal/www/portal/slice-jacks.php
@@ -60,6 +60,20 @@ if (isset($slice_expired) && convert_boolean($slice_expired) ) {
   $disable_buttons_str = " disabled";
 }
 
+if (! isset($all_ams)) {
+  $am_list = get_services_of_type(SR_SERVICE_TYPE::AGGREGATE_MANAGER);
+  $all_ams = array();
+  foreach ($am_list as $am)
+    {
+      $single_am = array();
+      $service_id = $am[SR_TABLE_FIELDNAME::SERVICE_ID];
+      $single_am['name'] = $am[SR_TABLE_FIELDNAME::SERVICE_NAME];
+      $single_am['url'] = $am[SR_TABLE_FIELDNAME::SERVICE_URL];
+      $single_am['urn'] = $am[SR_TABLE_FIELDNAME::SERVICE_URN];
+      $all_ams[$service_id] = $single_am;
+    }
+}
+
 // print_r( $all_ams);
 
 // For comparing member records by role (low roles come before high roles)
@@ -117,7 +131,7 @@ function build_agg_table_on_slicepg()
      $output .= "<td colspan='2'>";
      $output .= "<button title='Get summary status for resources at selected aggregates.' onClick=\"getCheckedStatus();\"><b>Ready?</b></button>";
      $output .= "<button title='Login info, etc. for resources at selected aggregates.' onClick=\"doOnChecked('$listres_url');\"><b>Resource Details</b></button>";
-     $output .= "<button title='Get status of individual resources at selected aggregates.' onClick=\"doOnChecked('$status_url')\"><b>Resource Status</b></button>";
+     //     $output .= "<button title='Get status of individual resources at selected aggregates.' onClick=\"doOnChecked('$status_url')\"><b>Resource Status</b></button>";
      $output .= "<button title='Delete resources at selected aggregates.' onClick=\"doOnChecked('confirm-sliverdelete.php?slice_id=" . $slice_id . "', true)\"><b>Delete Resources</b></button>";
      $output .= "</td></tr>\n";
 
@@ -179,7 +193,7 @@ function build_agg_table_on_slicepg()
       $output .= "<td colspan='2' class='hide status_buttons'><div>";
       $output .= "<button  id='add_button_".$am_id."' title='Add resources at this aggregate.' onClick=\"window.location='".$add_url."&am_id=".$am_id."'\" $add_slivers_disabled $disable_buttons_str><b>Add</b></button>\n";
 	    $output .= "<button  id='details_button_".$am_id."' title='Login info, etc. for resources at this aggregate.' onClick=\"window.location='".$listres_url."&am_id=".$am_id."'\" $get_slice_credential_disable_buttons><b>Details</b></button>\n";
-      $output .= "<button id='status_button_".$am_id."' title='Get status of individual resources at this aggregate.' onClick=\"window.location='".$status_url."&am_id=".$am_id."'\" $get_slice_credential_disable_buttons><b>Status</b></button>\n";
+#      $output .= "<button id='status_button_".$am_id."' title='Get status of individual resources at this aggregate.' onClick=\"window.location='".$status_url."&am_id=".$am_id."'\" $get_slice_credential_disable_buttons><b>Status</b></button>\n";
 	    $output .= "<button  id='delete_button_".$am_id."' title='Delete resources at this aggregate.' onClick=\"window.location='confirm-sliverdelete.php?slice_id=".$slice_id."&am_id=".$am_id."'\" ".$delete_slivers_disabled." $disable_buttons_str><b>Delete</b></button>\n";
       $output .= "</div></td></tr>";
       
@@ -210,6 +224,64 @@ function build_agg_table_on_slicepg()
      $output .= "</table></td>";
      $output .= "</tr></table>";
      return $output;
+}
+
+if (! isset($sa_url)) {
+  $sa_url = get_first_service_of_type(SR_SERVICE_TYPE::SLICE_AUTHORITY);
+}
+
+if (! isset($ma_url)) {
+  $ma_url = get_first_service_of_type(SR_SERVICE_TYPE::MEMBER_AUTHORITY);
+}
+
+if (isset($slice)) {
+  //  $slice_name = $slice[SA_ARGUMENT::SLICE_NAME];                            
+  //  error_log("SLICE  = " . print_r($slice, true));                           
+  $slice_desc = $slice[SA_ARGUMENT::SLICE_DESCRIPTION];
+  $slice_creation_db = $slice[SA_ARGUMENT::CREATION];
+  $slice_creation = dateUIFormat($slice_creation_db);
+  $slice_expiration_db = $slice[SA_ARGUMENT::EXPIRATION];
+  $slice_expiration = dateUIFormat($slice_expiration_db);
+  $slice_date_expiration = dateOnlyUIFormat($slice_expiration_db);
+  $slice_urn = $slice[SA_ARGUMENT::SLICE_URN];
+  $slice_owner_id = $slice[SA_ARGUMENT::OWNER_ID];
+  $owner = $user->fetchMember($slice_owner_id);
+  $slice_owner_name = $owner->prettyName();
+  $owner_email = $owner->email();
+
+  $project_name = $project[PA_PROJECT_TABLE_FIELDNAME::PROJECT_NAME];
+  //error_log("slice project_name result: $project_name\n");                    
+  // Fill in members of slice member table                                      
+  $members = get_slice_members($sa_url, $user, $slice_id);
+  $member_names = lookup_member_names_for_rows($ma_url, $user, $members,
+                                               SA_SLICE_MEMBER_TABLE_FIELDNAME::MEMBER_ID);
+
+  //find only ams that slice has resources on
+  $slivers = lookup_sliver_info_by_slice($sa_url, $user, $slice_urn);
+  //find aggregates to be able to return just am_id
+  $all_aggs = get_services_of_type(SR_SERVICE_TYPE::AGGREGATE_MANAGER);
+  $aggs_with_resources = Array();
+
+  //do the comparison and find ams
+  foreach($slivers as $sliver)
+    {
+      foreach($all_aggs as $agg)
+	{
+	  if($sliver[SA_SLIVER_INFO_TABLE_FIELDNAME::SLIVER_INFO_AGGREGATE_URN] == $agg[SR_TABLE_FIELDNAME::SERVICE_URN])
+	    {
+	      $aggs_with_resources[] = $agg[SR_TABLE_FIELDNAME::SERVICE_ID];
+	      break;
+	    }
+	}
+    }
+  //return unique ids
+  $slice_ams = array_unique($aggs_with_resources, SORT_REGULAR);
+
+} else {
+  print "Unable to load slice<br/>\n";
+  $_SESSION['lasterror'] = "Unable to load slice";
+  relative_redirect("home.php");
+  exit();
 }
 
 include("jacks-app.php");
