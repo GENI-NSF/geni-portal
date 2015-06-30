b32fe770c16ef54b051c23be5b913ea6dd2b7646
Charlie Meyer <chmeyer@bbn.com>
2015-06-30 15:58:41 -0400

    Add slice sorting to the admin page, more visual tweaks.
    Can sort by various parameters like expiration date and slice name.
    Add "urgency colors" to displayed expiration dates along with synonymous
    iconography for the colorblind.

--- portal/www/portal/dashboard.php
+++ portal/www/portal/dashboard.php
@@ -53,13 +53,20 @@ include("tool-showmessage.php");
     display: inline;
   }
 
+  .floatright {
+    float: right;
+  }
+
+  .floatleft {
+    float: left;
+  }
+
   .slicebox {
-    width: 330px;
+    width: 360px;
     padding: 0px;
-    margin: 15px 30px 15px 0px;
+    margin: 15px 10px 15px 20px;
     border-radius: 3px;
     border: 2px solid #5F584E;
-    float: left;
   }
 
   .sliceboxinside {
@@ -84,7 +91,6 @@ include("tool-showmessage.php");
     background-color: #5F584E;
     padding: 5px;
     border-radius: 0px;
-    margin-top: 15px;
   }
 
   select:focus {
@@ -92,81 +98,182 @@ include("tool-showmessage.php");
   }
 
   option {
-    font-family: 'Open Sans';
     font-size: 16px;
-    color: #ffffff;
-    background-color: #5F584E;
   }
 
   option:focus {
       outline-width: 0;
   }
 
+  #projectswitch {
+    margin: 20px 15px;
+  }
+
+  #sortby {
+    margin: 20px 15px;
+  }
+
+  .projectinfo {
+    margin: 20px;
+    font-weight: bold;
+  }
+
+  #logtable {
+     margin-left: 15px;
+  }
+
+  .slicelink:hover {
+    cursor: pointer;
+    text-decoration: underline;
+  }
+
+  .expirationicon {
+    height: 16px;
+    width: 16px;
+    vertical-align: bottom;
+    margin-left: 5px; 
+  }
+
+  td {
+    font-family: 'Open Sans' !important;
+  }
+
+  button {
+    width: 105px;
+  }
+
+  @media (max-width: 600px) {
+    .dashtext {
+      display: block;
+      text-align: center;
+      margin: 5px auto !important;
+    }
+
+    .floatright, .floatleft {
+      float: none;
+      margin: 0 auto;
+    }
+
+    .slicebox {
+      margin: 20px auto;
+    }
+
+    #dashboardtools {
+      text-align: center;
+    }
+
+    #projectcontrols {
+      text-align: center;
+    }
+
+    #projectswitch {
+      margin: 5px 15px;
+    }
+
+    .projectinfo {
+      margin: 10px;
+      text-align: center;
+    }
+
+    .slicelink {
+      text-decoration: underline;
+    }
+
+    #logtable {
+      margin-left: 0px;
+    }
+  }
+
 </style>
 
 <script>
   $(document).ready(function(){
-    $(".slicebox").hide();
-    if (localStorage.lastproj){
-      if (localStorage.lastproj == "__ALL") {
-        show_all_slices();
-      } else {
-        $("#projectswitch").val(localStorage.lastproj);
-        if($("." + localStorage.lastproj + "slice").length == 0) {
-          $("#slicearea").append("<h3 class='dashtext noslices'>No slices for project " + localStorage.lastproj + ".</h3>");
-        }
-      }
-    }
 
-    $("." + $("#projectswitch").val() + "slice").show();
+    if (localStorage.lastselection){
+      $("#projectswitch").val(localStorage.lastselection);
+    }
+    if (localStorage.lastsortby){
+      $("#lastsortby").val(localStorage.lastselection);
+    }
+    show_slices($("#projectswitch").val(), $("#sortby").val());
 
     $("#projectswitch").change(function(){
-      $('#showallslices').removeAttr('disabled');
-      $('#showallslices').html("Show all slices");
-      $(".noslices").remove();
-      $("#whatslices").html("Slices for project:");
-      $(".projectinfo").hide();
-      new_name = $(this).val()
-      $("#" + new_name + "info").show();
-      $("#" + new_name + "info").css("float", "right");
+      show_slices($(this).val(), $("#sortby").val());
+    });
+
+    $("#sortby").change(function(){
+      show_slices($("#projectswitch").val(), $(this).val());
+    });
+
+    $('#ascendingcheck').change(function() {
+      sort_slices($("#sortby").val(), this.checked);       
+    });
+
+
+    function show_slices(selection, sortby) {
       $(".slicebox").hide();
-      $("." + new_name + "slice").show();
-      localStorage.setItem("lastproj", new_name);
-      if($("." + new_name + "slice").length == 0) {
-        $("#slicearea").append("<h3 class='dashtext noslices'>No slices for project " + new_name + ".</h3>");
-      }
-    });
-
-    $("#showallslices").click(function() {
-      localStorage.setItem("lastproj", "__ALL");
-      show_all_slices();
-    });
-
-    function show_all_slices() {
       $(".noslices").remove();
-      $("#showallslices").attr('disabled', 'disabled');
-      $("#showallslices").html("Showing all slices");
-      $(".slicebox").show();
       $(".projectinfo").hide();
-      $("#whatslices").html("Filter by project: ");
+      localStorage.setItem("lastselection", selection);
+      localStorage.setItem("lastsortby", sortby);
+
+      if (is_all(selection)) {
+        project_name = "";
+        class_name = selection;
+        no_slice_msg = "No slices to display.";
+      } else {
+        project_name = selection;
+        class_name = selection + "slices";
+        no_slice_msg = "No slices for project" + project_name;
+        $("#" + project_name + "info").show();
+        $("#" + project_name + "info").css("float", "right");
+      }
+
+      $("." + class_name).show();
+      sort_slices(sortby, $("#ascendingcheck").prop("checked"));
+      if($("." + class_name).length == 0) {
+        $("#slicearea").append("<h3 class='dashtext noslices'>" + no_slice_msg + "</h3>");
+      }
     }
+
+    function is_all(class_name) {
+      return class_name[0] == '-';
+    }
+
+    function sort_slices(attr, ascending) {
+      numberical_attrs = ['sliceexp', 'resourceexp', 'resourcecount'];
+      sorted_slices = $("#slicearea").children().sort(function(a, b) {
+        if ($.inArray(attr, numberical_attrs) != -1) { // is it a numerical attribute?
+          vA = parseInt($(a).attr(attr));
+          vB = parseInt($(b).attr(attr));
+        } else {
+          vA = $(a).attr(attr);
+          vB = $(b).attr(attr); 
+        }
+        if(ascending) {
+          return (vA < vB) ? -1 : (vA > vB) ? 1 : 0;
+        } else {
+          return (vA < vB) ? 1 : (vA > vB) ? -1 : 0;
+        }
+      });
+      $("#slicearea").append(sorted_slices);
+    }
+
   });
 </script>
 
 <?php
-
   $retVal  = get_project_slice_member_info( $sa_url, $ma_url, $user, True);
   $project_objects = $retVal[0];
   $slice_objects = $retVal[1];
   $member_objects = $retVal[2];
   $project_slice_map = $retVal[3];
   $project_activeslice_map = $retVal[4];
-
 ?>
 
 <div id="dashboardheader">
-  <h1 class="dashtext" style="float:left; ">GENI Dashboard</h1>
-  <div id="dashboardtools" style="float:right; vertical-align: middle;">
+  <h1 class="dashtext floatleft">GENI Dashboard</h1>
+  <div id="dashboardtools" class="floatright" style="vertical-align: middle; border: 2px solid #5F584E;">
     <h3 class="dashtext" style="color:white !important;">Manage: </h3>
     <button onclick="window.location='profile.php#accountsummary'">Account</button>
     <button onclick="window.location='profile.php#ssh'">SSH Keys</button>
@@ -175,7 +282,7 @@ include("tool-showmessage.php");
   </div>
 </div>
 
-<div style="clear:both; margin-top: 50px;">
+<div style="clear:both;">
 
 <?php
   if (count($project_objects) == 0){
@@ -183,7 +290,7 @@ include("tool-showmessage.php");
   } else {
     $lead_names = lookup_member_names_for_rows($ma_url, $user, $project_objects, 
                                                 PA_PROJECT_TABLE_FIELDNAME::LEAD_ID);
-    $project_options = "<select id='projectswitch' style='margin: 0px 15px;'>";
+    $project_options = "<select id='projectswitch'>";
     $project_info = "";
     $show_info = "";
     foreach ($project_objects as $project) {
@@ -192,13 +299,20 @@ include("tool-showmessage.php");
       $lead_id = $project[PA_PROJECT_TABLE_FIELDNAME::LEAD_ID];
       $lead_name = $lead_names[$lead_id];
       $create_slice_button = "<button onClick='window.location=\"createslice.php?project_id=$project_id\"'><b>new slice</b></button>";
-      $project_options .= "<option value='$project_name'>$project_name</option>";
-      $project_info .= "<div $show_info class='projectinfo' id='{$project_name}info' style='float:right; vertical-align: bottom !important;'>";
-      $project_info .= "<p style='font-weight: bold;'><b>Project Lead:</b> $lead_name | $create_slice_button</p></div>";
+      $selected_project = $show_info == "" ? "selected" : "";
+      $project_options .= "<option $selected_project value='{$project_name}'>Project: $project_name</option>";
+      $project_info .= "<div $show_info class='floatright projectinfo' id='{$project_name}info'>";
+      $project_info .= "<b>Project Lead:</b> $lead_name | $create_slice_button</div>";
       $show_info = "style='display:none;'";
     }
+    $project_options .= "<option value='-MY-'>All slices I lead</option>";
+    $project_options .= "<option value='-THEIR-'>All slices I don't lead</option>";
+    $project_options .= "<option value='-ALL-'>All slices</option>";
     $project_options .= "</select>";
-    print "<div style='float:left;'><h3 class='dashtext' id='whatslices'>Slices for project:</h3>$project_options<button id='showallslices'>Show all</button></div>";
+    print "<div id='projectcontrols' class='floatleft'><h4 class='dashtext'>Filter by:</h4>$project_options |"; 
+    print "<h4 class='dashtext' style='margin-left: 15px !important;'>Sort by:</h4><select id='sortby'><option value='slicename'>Slice name</option><option value='sliceexp'>Slice expiration</option>";
+    print "<option value='resourceexp'>Next resource expiration</option></select>";
+    print "<input type='checkbox' id='ascendingcheck' value='ascending' checked>Sort ascending<br></div>";
     print $project_info;
   }
 ?>
@@ -223,49 +337,79 @@ include("tool-showmessage.php");
                                                       SA_SLICE_TABLE_FIELDNAME::OWNER_ID);
   }
 
-  function make_slice_box($slice_name, $lead_name, $project_name, $resource_count, $slice_exp, $resource_exp, 
-                          $add_url, $remove_url) {
-    print "<div class='slicebox {$project_name}slice'><div class='sliceboxinside'><table>";
+  function make_slice_box($slice_name, $whose_slice, $slice_url, $lead_name, $project_name, $resource_count, 
+                          $slice_exp, $resource_exp, $add_url, $remove_url) {
+    print "<div class='floatleft slicebox $whose_slice {$project_name}slices' slicename='$slice_name' sliceexp='$slice_exp' resourceexp='$resource_exp'>";
+    print "<div class='sliceboxinside'><table>";
+    $resource_exp_icon = "";
     if ($resource_count > 0){
-      $resource_info = "$<b>resource_count</b> resources, next one expires in <b>$resource_exp</b>";
+      $plural = $resource_count == 1 ? "" : "s";
+      $resource_exp_string = get_time_diff_string($resource_exp);
+      $resource_exp_color = get_urgency_color($resource_exp);
+      $resource_info = "<b>$resource_count</b> resource{$plural}, next expiration in <b style='color: $resource_exp_color'>$resource_exp_string</b>";
+      $resource_exp_icon = "<img class='expirationicon' alt='$resource_exp_color resource expiration icon' src='/common/${resource_exp_color}.png'/>";
     } else {
       $resource_info = "<i>No resources for this slice</i>";
     }
-    $slice_info = "Slice expires in <b>$slice_exp</b>";
-    print "<tr><td colspan='2' style='text-align:center; background-color: #F57F21;'><h5 class='dashtext' style='color:white !important;'>$slice_name <br> lead: $lead_name</h5></td><tr>";
-    print "<tr><td>$slice_info</td>";
+    $slice_exp_string = get_time_diff_string($slice_exp);
+    $slice_exp_color = get_urgency_color($slice_exp);
+    $slice_info = "Slice expires in <b style='color: $slice_exp_color'>$slice_exp_string</b>";
+    $slice_exp_icon = "<img class='expirationicon' alt='$slice_exp_color slice expiration icon' src='/common/${slice_exp_color}.png'/>";
+    print "<tr><td colspan='3' style='text-align:center; background-color: #F57F21;'>";
+    print "<h5 class='slicelink dashtext' onclick='window.location=\"$slice_url\"' style='color:white !important;'>$slice_name</h5><br>";
+    print "<h6 class='dashtext' style='color:white !important;'>lead: $lead_name</h6></td><tr>";
+    print "<tr><td style='width:180px;'>$slice_info</td><td style='vertical-align: center;'>$slice_exp_icon</td>";
     print "<td rowspan='2' style='text-align:center; border-left: 1px solid white;'>";
-    
     print "<button onclick='window.location=\"$add_url\"'>Add resources</button><br><button onclick='window.location=\"$remove_url\"'>Remove resources</button></td></tr>";
-    print "<tr><td style='border-left: 1px solid white;'>$resource_info</td></tr>";
+    print "<tr><td style='border-left: 1px solid white; height:55px;'>$resource_info</td><td style='vertical-align: center;''>$resource_exp_icon</td></tr>";
     print "</table></div></div>";
   }
   
-  function get_time_diff_string($exp_date) {
-    $now = new DateTime('now');
-    $exp_datetime = new DateTime($exp_date);
-    if ($exp_datetime < $now) {
-      return "This is expired";
-    } 
-    $interval = date_diff($exp_datetime, $now);
-    $num_hours = $interval->days * 24 + $interval->h;
+  function get_time_diff_string($num_hours) {
     if ($num_hours < 48) {
       return "$num_hours hours";
     } else {
-      return "{$interval->days} days";
+      $num_days =  $num_hours / 24;
+      $num_days = (int) $num_days;
+      return "$num_days days";
+    }
+  }
+  
+  function get_time_diff($exp_date) {
+    $now = new DateTime('now');
+    $exp_datetime = new DateTime($exp_date);
+    $interval = date_diff($exp_datetime, $now);
+    $num_hours = $interval->days * 24 + $interval->h;
+    return $num_hours;
+  }
+
+  function get_urgency_color($num_hours) {
+    if ($num_hours < 24) { 
+      return "red";
+    } else if ($num_hours < 48) {
+      return "orange";
+    } else {
+      return "green";
     }
   }
 
+  $user_id = $user->account_id;
   foreach ($slice_objects as $slice) {
     $slice_id = $slice[SA_SLICE_TABLE_FIELDNAME::SLICE_ID];
     $slice_urn = $slice[SA_ARGUMENT::SLICE_URN];
     $slice_name = $slice[SA_ARGUMENT::SLICE_NAME];
     $slice_owner_id = $slice[SA_ARGUMENT::OWNER_ID];
+    if ($slice_owner_id == $user_id) {
+      $whose_slice = "-MY- -ALL-";
+    } else {
+      $whose_slice = "-THEIR- -ALL-";
+    }
     $slice_exp_date = $slice[SA_ARGUMENT::EXPIRATION];
     $args['slice_id'] = $slice_id;
     $query = http_build_query($args);
     $add_resource_url = "slice-add-resources-jacks.php?" . $query;
     $delete_resource_url = "confirm-sliverdelete.php?" . $query;
+    $slice_url = "slice.php?" . $query;
     $slice_project_id = $slice[SA_SLICE_TABLE_FIELDNAME::PROJECT_ID];
     if (!array_key_exists($slice_project_id, $project_objects)) {
       $slice_project_name = "-Expired Project-";
@@ -278,10 +422,10 @@ include("tool-showmessage.php");
               $slice_id);
 
     $slivers = lookup_sliver_info_by_slice($sa_url, $user, $slice_urn);
-    $slice_exp_info = get_time_diff_string($slice_exp_date);
+    $slice_exp = get_time_diff($slice_exp_date);
 
     if (count($slivers) == 0) {
-      $resource_exp_info = "";
+      $resource_exp = -999;
     } else {
       $first_sliver = reset($slivers);
       $next_exp = new DateTime($first_sliver[SA_SLIVER_INFO_TABLE_FIELDNAME::SLIVER_INFO_EXPIRATION]);
@@ -291,10 +435,12 @@ include("tool-showmessage.php");
           $next_exp = $this_date;
         }
       }
-      $resource_exp_info = get_time_diff_string($next_exp); 
+      $resource_exp = get_time_diff(dateUIFormat($next_exp)); 
     }
-    make_slice_box($slice_name, $slice_owner_names[$slice_owner_id], $slice_project_name, count($slivers), $slice_exp_info, $resource_exp_info, $add_resource_url, $delete_resource_url);
+    make_slice_box($slice_name, $whose_slice, $slice_url, $slice_owner_names[$slice_owner_id], $slice_project_name,
+                   count($slivers), $slice_exp, $resource_exp, $add_resource_url, $delete_resource_url);
   }
+
   if(count($slice_objects) == 0 ){
     print "<h4 class='dashtext'>No slices</h4>";
   }
@@ -303,19 +449,19 @@ include("tool-showmessage.php");
 </div>
 
 <div style="clear:both;">&nbsp;</div>
-<h2 class="dashtext">GENI Messages</h2>
-<p>Showing logs for the last 
+<h2 class="dashtext">GENI Messages</h2><br>
+<h4 class='dashtext'>Showing logs for the last 
 <select id="loglength" onchange="getLogs(this.value);">
   <option value="24">day</option>
   <option value="48">2 days</option>
   <option value="72">3 days</option>
   <option value="168">week</option>
-</select></p>
+</select></h4>
 
 <script type="text/javascript">
   $(document).ready(function(){
     if(localStorage.loghours){
-      $("#loglength").val(localStorage.logHours);
+      $("#loglength").val(localStorage.loghours);
       getLogs(localStorage.loghours);
     } else {
       getLogs("24");
@@ -324,12 +470,12 @@ include("tool-showmessage.php");
   function getLogs(hours){
     localStorage['loghours'] = hours;
     $.get("do-get-logs.php?hours="+hours, function(data) {
-      $('#log_table').html(data);
+      $('#logtable').html(data);
     });
   }
 </script>
 <div class="tablecontainer">
-  <table id="log_table" style="border: 2px solid #5F584E;"></table>
+  <table id="logtable" style="border: 2px solid #5F584E;"></table>
 </div>
 
 <?php
