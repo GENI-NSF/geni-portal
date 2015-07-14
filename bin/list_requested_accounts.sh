251ef56b399fcb07a9fa9771db9a109d6b71af5f
Marshall Brinn <mbrinn@bbn.com>
2015-07-14 12:13:18 -0400

    Remove scripts that depend on account/identity

--- bin/list_requested_accounts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# list all accounts of status requested
-
-echo "Requested Accounts:"
-echo "select username from account where status = 'requested'" | psql -U portal -h localhost portal
-
-
