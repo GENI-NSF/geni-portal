26817b2b887db430dff8fecd4289cb021b8a9835
Marshall Brinn <mbrinn@bbn.com>
2015-07-16 07:26:37 -0400

    Add revert-9.sql script, add update-9 and revert-9 scripts to Makefile.sm

---
+++ portal/db/postgresql/revert-9.sql
@@ -0,0 +1,16 @@
+-- -------------------
+-- Apply changes to rename abac, identity, account portal tables
+-- (make inaccessible from existing code, but archive)
+-- See geni-portal #299
+-- -------------------
+
+alter table abac_assertion_299 rename to abac_assertion;
+alter table abac_299 rename to abac;
+alter table account_299 rename to account;
+alter table account_privilege_299 rename to account_privilege;
+alter table identity_attribute_299 rename to identity_attribute;
+alter table identity_299 rename to identity;
+alter table_299 requested_account rename to requested_account;
+alter table shib_attribute_299 rename to shib_attribute;
+alter table slice_299 rename to slice;
+
