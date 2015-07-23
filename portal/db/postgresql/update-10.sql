17b4ba8fec931198bc0c3bafcc7a5b21f4c56c96
Tom Mitchell <tmitchell@bbn.com>
2015-07-23 16:12:54 -0400

    Drop tables in correct order.
    Drop tables ordered to avoid dependency errors. Add 'if exists' clause
    so script can be run again. Suppress warnings. Stop on errors.

--- portal/db/postgresql/update-10.sql
+++ portal/db/postgresql/update-10.sql
@@ -3,13 +3,19 @@
 -- See geni-portal #299
 -- -------------------
 
-drop table abac_assertion;
-drop table abac;
-drop table account;
-drop table account_privilege;
-drop table identity_attribute;
-drop table identity;
-drop view requested_account;
-drop table shib_attribute;
-drop table slice;
+-- avoid innocuous NOTICEs about automatic sequence creation
+set client_min_messages='WARNING';
 
+-- Tell psql to stop on an error. Default behavior is to proceed.
+\set ON_ERROR_STOP 1
+
+
+drop table if exists abac_assertion;
+drop table if exists abac;
+drop view  if exists requested_account;
+drop table if exists identity_attribute;
+drop table if exists identity;
+drop table if exists account_privilege;
+drop table if exists slice;
+drop table if exists account;
+drop table if exists shib_attribute;
