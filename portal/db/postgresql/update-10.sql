08e55acda800ea263e2a685f609353eedcd779b7
Marshall Brinn <mbrinn@bbn.com>
2015-07-23 15:13:50 -0400

    Change update-10 to drop tables not rename tables

--- portal/db/postgresql/update-10.sql
+++ portal/db/postgresql/update-10.sql
@@ -1,16 +1,15 @@
 -- -------------------
--- Apply changes to rename abac, identity, account portal tables
--- (make inaccessible from existing code, but archive)
+-- Drop abac, identity, account, abac, shib portal tables
 -- See geni-portal #299
 -- -------------------
 
-alter table abac_assertion rename to abac_assertion_299;
-alter table abac rename to abac_299;
-alter table account rename to account_299;
-alter table account_privilege rename to account_privilege_299;
-alter table identity_attribute rename to identity_attribute_299;
-alter table identity rename to identity_299;
-alter table requested_account rename to requested_account_299;
-alter table shib_attribute rename to shib_attribute_299;
-alter table slice rename to slice_299;
+drop table abac_assertion;
+drop table abac;
+drop table account;
+drop table account_privilege;
+drop table identity_attribute;
+drop table identity;
+drop view requested_account;
+drop table shib_attribute;
+drop table slice;
 
