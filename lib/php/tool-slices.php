0b950f67d54617c830ef3424d5b765bd21592f11
Charlie Meyer <chmeyer@bbn.com>
2015-08-17 17:37:54 -0400

    Comment out duplicate code in tool-slices for dashboard integration.
    Including tool-slices on the dashboard was causing a
    redeclaration of "get_urgency_color()" error.

--- lib/php/tool-slices.php
+++ lib/php/tool-slices.php
@@ -171,26 +171,26 @@ function make_slice_table($slicelist) {
     print "</div>";
 }
 
-/* returns a color based on how close the date is to now 
-   red = < 1 day
-   orange =  < 2 days 
-   green = > 2 days      */
-function get_urgency_color($exp_date){
-  $now = new DateTime('now');
-  $exp_datetime = new DateTime($exp_date);
-  if ($exp_datetime < $now) {
-    return "red; text-decoration: underline;";
-  } 
-  $interval = date_diff($exp_datetime, $now);
-  $num_hours = $interval->days * 24 + $interval->h;
-  if ($num_hours < 24) { 
-    return "red";
-  } else if ($num_hours < 48) {
-    return "orange";
-  } else {
-    return "green";
-  }
-}
+// /* returns a color based on how close the date is to now 
+//    red = < 1 day
+//    orange =  < 2 days 
+//    green = > 2 days      */
+// function get_urgency_color($exp_date){
+//   $now = new DateTime('now');
+//   $exp_datetime = new DateTime($exp_date);
+//   if ($exp_datetime < $now) {
+//     return "red; text-decoration: underline;";
+//   } 
+//   $interval = date_diff($exp_datetime, $now);
+//   $num_hours = $interval->days * 24 + $interval->h;
+//   if ($num_hours < 24) { 
+//     return "red";
+//   } else if ($num_hours < 48) {
+//     return "orange";
+//   } else {
+//     return "green";
+//   }
+// }
 
 function list_slice($slice,$user) {
   global $project_objects, $slice_owner_names;
