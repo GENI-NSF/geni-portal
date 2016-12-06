549cd93916f03eb2a664b36c7f63b2319114e146
Tom Mitchell <tcmitchell@users.noreply.github.com>
2016-12-06 16:11:43 -0500

    Use $portal_from_email in do-upload-project-members

--- portal/www/portal/do-upload-project-members.php
+++ portal/www/portal/do-upload-project-members.php
@@ -117,7 +117,10 @@ foreach($selections as $email_name => $attribs) {
     $userEmail = $user->email();
     $name = $user->prettyName();
 
-    $headers = "Reply-To: $userEmail" . "\r\n" . "From: \"$name (via the GENI Portal)\" <www-data@gpolab.bbn.com>\r\nContent-Type: text/plain; charset=UTF-8\r\nContent-Transfer-Encoding: 8bit";
+    $headers = "Reply-To: $userEmail\r\n";
+    $headers .= "From: \"$name (via the GENI Portal)\" <$portal_from_email>\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+    $headers .= "Content-Transfer-Encoding: 8bit";
 
     mail($email, $email_subject, $email_text,
 	 $headers,
